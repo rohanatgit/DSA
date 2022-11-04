23627861a750929c574e92d33d9a84ad6ad561d0
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-11-04 09:32:02 +0530

    Fuction_Demo2

---
+++ src/Lec7/Fuction_Demo2.java
@@ -0,0 +1,15 @@
+package Lec7;
+
+public class Fuction_Demo2 {
+    public static void main(String[] args) {
+        System.out.println("Hey");
+        int a=9;
+        int b=23;
+        System.out.println(add(a,b));
+        System.out.println("bye");
+    }
+    public static int add(int a,int b){
+        int c=a+b;
+        return c;
+    }
+}
