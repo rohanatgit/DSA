c44c31cead44455dbd0f4c0d7f7988c9c658cfba
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-11-04 09:26:45 +0530

    Fuction_Demo

---
+++ src/Lec7/Fuction_DEmo.java
@@ -0,0 +1,14 @@
+package Lec7;
+
+public class Fuction_DEmo {
+    public static void main(String[] args) {
+        System.out.println(":hii");
+        addtwonumber();
+    }
+    public static void addtwonumber(){
+        int a=9;
+        int b=11;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
