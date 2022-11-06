d4f4444bd5807ef4841ac565c93ffc8032cdc8a9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-11-06 22:48:22 +0530

    Revesal_Algo

---
+++ src/Lec9/Revesal_Algo.java
@@ -0,0 +1,29 @@
+package Lec9;
+
+public class Revesal_Algo {
+    public static void main(String[] args) {
+       int arr[]={1,2,3,4,5,6,7};
+       int k=3;
+       rotate_Array(arr,k);
+       for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i]);
+       }
+    }
+    public static void rotate_Array(int arr[],int k){
+            k=k%arr.length;
+            int n=arr.length;
+            Reverse(arr,0,n-k-1);//1st n-k element ko revsrse keya
+            Reverse(arr,n-k,n-1);//last k element reverse
+            Reverse(arr,0,n-1);//hole reverse
+    }
+    public static void Reverse(int[] arr,int i,int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+
+    }
+}
