e6bac82de2c17e7fa041dab2373a58f8dec644a9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-11-07 00:06:34 +0530

    Bubble_Sort

---
+++ src/Lec10/Bubble_Sort.java
@@ -0,0 +1,23 @@
+package Lec10;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        int[] arr={1,3,2,7,9,4};
+        Sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void Sort(int[] arr){
+        //Pass
+        for(int pass=1;pass<arr.length;pass++){
+            for(int i=0;i<arr.length-pass;i++){
+                if(arr[i]>arr[i+1]){
+                    int t=arr[i];
+                    arr[i]=arr[i+1];
+                    arr[i+1]=t;
+                }
+            }
+        }
+    }
+}
