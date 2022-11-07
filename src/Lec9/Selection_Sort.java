06593b62ba528b0cc821bd32fedd6b0040d5acad
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-11-07 20:09:00 +0530

    Selection_Sort

---
+++ src/Lec9/Selection_Sort.java
@@ -0,0 +1,24 @@
+package Lec9;
+
+public class Selection_Sort {
+    public static void main(String[] args) {
+       int arr[]={1,3,4,5,6,3,68};
+       selectionsort(arr);
+       for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i]+" ");
+       }
+    }
+    public static void selectionsort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int min_idx=i;//index min
+             for(int j=i+1;j<arr.length;j++){
+                 if(arr[j]<arr[min_idx]){
+                     min_idx=j;
+                 }
+             }
+             int t=arr[i];
+             arr[i]=arr[min_idx];
+             arr[min_idx]=t;
+        }
+    }
+}
