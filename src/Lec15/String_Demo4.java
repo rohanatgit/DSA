f327185a9d0676277ae9728ae54e3ffe35b459cc
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-11-17 22:20:53 +0530

    String_Demo4

---
+++ src/Lec15/String_Demo4.java
@@ -0,0 +1,10 @@
+package Lec15;
+
+public class String_Demo4 {
+    public static void main(String[] args) {
+        String str1="helo";
+        System.out.println(str1+(10+30));//helo40
+        System.out.println(str1+10+30);//helo1030 print hoga
+    }
+}
+
