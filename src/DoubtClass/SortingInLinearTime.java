fe0f1329c0dcd758033881e38873caf9a83d3c40
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-12-21 14:51:22 +0530

    SortingInLinearTime

--- src/DoubtClass/SortingInLinearTime.java
+++ src/DoubtClass/SortingInLinearTime.java
@@ -1,5 +1,7 @@
 package DoubtClass;
 
+import java.util.Scanner;
+
 public class SortingInLinearTime {
     public static void sorting(int[] arr){
         int left=0;
@@ -24,6 +26,17 @@ public class SortingInLinearTime {
         }
     }
     public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+       int n=sc.nextInt();
+       int arr[]=new int[n];
+       for(int i=0;i<arr.length;i++){
+           arr[i]=sc.nextInt();
+
+       }
+       sorting(arr);
+       for(int i=0;i<arr.length;i++){
+           System.out.println(arr[i]+" ");
+       }
 
     }
 }
