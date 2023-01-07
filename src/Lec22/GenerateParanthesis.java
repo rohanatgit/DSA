5295b79ccbca45f2f85f20861ae2feb2811d257c
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-07 20:15:09 +0530

    GenerateParanthesis

---
+++ src/Lec22/GenerateParanthesis.java
@@ -0,0 +1,22 @@
+package Lec22;
+import java.util.*;
+public class GenerateParanthesis {
+    public static void generateparathsis(int n,int opening,int closing,String ans,List<String> list){
+        if(opening==n && closing==n){
+            //System.out.println(ans);
+            list.add(ans);
+            return;
+        }
+        if(opening<n) {
+            generateparathsis(n, opening + 1, closing, ans + "(",list);
+        }
+        if(closing<opening) {
+            generateparathsis(n, opening, closing + 1, ans + ")",list);
+        }
+    }
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        generateparathsis(4,0,0,"",list);
+        System.out.println(list);
+    }
+}
