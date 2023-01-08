ebf38a49099e306dffc78174e0994af68582c29d
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-08 18:57:41 +0530

    String Permutation

---
+++ src/Lec22/Permutation.java
@@ -0,0 +1,19 @@
+package Lec22;
+
+public class Permutation {
+    public static void printPermutation(String ques,String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return ;
+        }
+        for(int i=0;i<ques.length();i++){
+            String ros=ques.substring(0,i)+ques.substring(i+1);
+            char ch=ques.charAt(i);
+            printPermutation(ros,ans+ch);
+        }
+    }
+    public static void main(String[] args) {
+       String str="abc";
+       printPermutation(str,"");
+    }
+}
