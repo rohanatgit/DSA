919fe326ce76dcc870b9e8cb923cd935e1b3f2d0
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-18 19:23:48 +0530

    Combination_Sum Leetcode

--- src/Lec11/MaximumSubArray.java
+++ src/Lec11/MaximumSubArray.java
@@ -2,7 +2,7 @@ package Lec11;
 
 public class MaximumSubArray {
     public static void main(String[] args) {
-        int arr[]={-2,1,-3,-2,9,8};
+        int arr[]={1,-2,3,-2};
         System.out.println(maxsum(arr));
     }
     public static int maxsum(int[] arr){
