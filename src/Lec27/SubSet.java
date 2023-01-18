a5d7bcfd4f997cc873ce1fb5b1afc191b2fd3fa1
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-19 01:28:18 +0530

    SubSet Leetcode

---
+++ src/Lec27/SubSet.java
@@ -0,0 +1,25 @@
+package Lec27;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSet {
+    public static void main(String[] args) {
+        int [] arr={1,2,3};
+        List<Integer> ll=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        subset(arr,0,ll,ans);
+        System.out.println(ans);
+    }
+    public static void subset(int [] arr,int i,List<Integer>ll ,List<List<Integer>>ans){
+        if(i==arr.length){
+            //System.out.println(ans);
+            ans.add(new ArrayList<>(ll));
+            return ;
+        }
+        subset(arr,i+1,ll,ans);
+        ll.add(arr[i]);
+        subset(arr,i+1,ll,ans);
+        ll.remove(ll.size()-1);
+    }
+}
