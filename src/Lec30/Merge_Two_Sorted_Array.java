48aaa560b828dfa13f433845848c00ac429dcc41
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-22 13:40:30 +0530

    Merge_Two_Sorted_Array

--- src/Lec30/Merge_Two_Sorted_Array.java
+++ src/Lec30/Merge_Two_Sorted_Array.java
@@ -9,35 +9,35 @@ public class Merge_Two_Sorted_Array {
             System.out.print(ans[i]+" ");
         }
     }
-    public static int[] mergeSortedArray(int arr1[],int arr2[]){
-        int n=arr1.length;
-        int m=arr2.length;
-        int ans[]=new int[n+m];
-        int i=0; //arr1  track
-        int j=0;//arr2  track ralhega
-        int k=0;//ans
-        while(i<n && j<m){
-            if(arr1[i]<=arr2[j]){
-                ans[k]=arr1[i];
+    public static int[] mergeSortedArray(int arr1[],int arr2[]) {
+        int n = arr1.length;
+        int m = arr2.length;
+        int ans[] = new int[n + m];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) {
+                ans[k] = arr1[i];
                 k++;
                 i++;
-            }
-            else{
-                ans[k]=arr2[j];
+            } else {
+                ans[k] = arr2[j];
                 j++;
                 k++;
             }
+
         }
-        while(i<n){
-            ans[k]=arr1[i];
+        while (i < n) {
+            ans[k] = arr1[i];
             k++;
             i++;
         }
-        while(j<m){
-            ans[k]=arr2[j];
+        while (j < m) {
+            ans[k] = arr2[j];
             k++;
             j++;
         }
-        return ans;
+    return ans;
     }
 }
