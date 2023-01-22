a9f21b5440dff07dbf8d01c488b89d4e3ed373ff
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-22 13:13:36 +0530

    Merge_Two_Sorted_Array

---
+++ src/Lec30/Merge_Two_Sorted_Array.java
@@ -0,0 +1,43 @@
+package Lec30;
+
+public class Merge_Two_Sorted_Array {
+    public static void main(String[] args) {
+        int arr1[]={3,5,7,9,11};
+        int[] arr2={1,2,3,4,6,7};
+        int[] ans=mergeSortedArray(arr1,arr2);
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+    public static int[] mergeSortedArray(int arr1[],int arr2[]){
+        int n=arr1.length;
+        int m=arr2.length;
+        int ans[]=new int[n+m];
+        int i=0; //arr1  track
+        int j=0;//arr2  track ralhega
+        int k=0;//ans
+        while(i<n && j<m){
+            if(arr1[i]<=arr2[j]){
+                ans[k]=arr1[i];
+                k++;
+                i++;
+            }
+            else{
+                ans[k]=arr2[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<n){
+            ans[k]=arr1[i];
+            k++;
+            i++;
+        }
+        while(j<m){
+            ans[k]=arr2[j];
+            k++;
+            j++;
+        }
+        return ans;
+    }
+}
