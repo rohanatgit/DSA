af68037820e3c9b2ca19b735377d38c7aca4205e
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-26 11:23:42 +0530

    StringSubsequence

--- src/RecursionRevision/PrintSubSequence.java
+++ src/RecursionRevision/PrintSubSequence.java
@@ -1,9 +1,11 @@
 package RecursionRevision;
 
 public class PrintSubSequence {
+    static int count=0;
     public static void subsequence(String ques,String ans ){
         if(ques.length()==0){
             System.out.println(ans);
+            count++;
             return ;
         }
         char ch=ques.charAt(0);
@@ -13,5 +15,6 @@ public class PrintSubSequence {
     public static void main(String[] args) {
      String str="abc";
      subsequence(str,"");
+        System.out.println(count);
     }
 }
