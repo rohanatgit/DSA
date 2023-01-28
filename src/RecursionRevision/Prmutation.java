17923f3939c03f63620824fb9cd0c4cb1976fbb2
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-28 19:45:54 +0530

    String Permutation

---
+++ src/RecursionRevision/Prmutation.java
@@ -0,0 +1,19 @@
+package RecursionRevision;
+
+public class Prmutation {
+    public static void main(String[] args) {
+        String str="abc";
+     printpermutation(str,"");
+    }
+    public static void printpermutation(String ques,String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return ;
+        }
+        for(int i=0;i<ques.length();i++){
+             char ch=ques.charAt(i);
+            String ros= ques.substring(0,i)+ques.substring(i+1);
+            printpermutation(ros,ans+ch);
+        }
+    }
+}
