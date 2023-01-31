1bd76e0201b97f3674733f9153beba890fae45fa
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-01-31 20:03:24 +0530

    CoinPermutation

--- src/RecursionRevision/CoinPermutation.java
+++ src/RecursionRevision/CoinPermutation.java
@@ -2,6 +2,19 @@ package RecursionRevision;
 
 public class CoinPermutation {
     public static void main(String[] args) {
-
+       int amount=10;
+       int coin[]={2,3,5,6};
+       coin_Per(coin,amount,"");
+    }
+    public static void coin_Per(int coin[],int amount,String ans){
+        if(amount==0){
+            System.out.println(ans);
+            return ;
+        }
+        for(int i=0;i<coin.length;i++){
+            if(amount>=coin[i]){
+                coin_Per(coin,amount-coin[i],ans+coin[i]);
+            }
+        }
     }
 }
