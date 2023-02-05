9294150fb6296fad91c9152714a58cfdffc52d09
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-05 22:20:40 +0530

    Recursion

--- src/RecursionRevision/RatChases.java
+++ src/RecursionRevision/RatChases.java
@@ -3,6 +3,7 @@ package RecursionRevision;
 import java.util.Scanner;
 
 public class RatChases {
+    static boolean f=false;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -16,11 +17,18 @@ public class RatChases {
         }
         int ans[][] = new int[n][m];
         printPath(maze, 0, 0, ans);
+        if(f==false){
+            System.out.println("Path NO FOUND");
+        }
     }
 
     public static void printPath(char[][] maze, int cr, int cc, int[][] ans) {
         if(cc==maze[0].length-1 && cr==maze.length-1){
-            display(ans);
+            if(maze[cr][cc]=='0'){
+                ans[cr][cc]=1;
+                display(ans);
+                ans[cr][cc]=0;
+            }
             return ;
         }
         if (cc < 0 || cc >= maze[0].length || cr < 0 || cr >= maze.length || maze[cr][cc] == 'X') {
