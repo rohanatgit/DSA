da975139b40d4b07abedf3bf230a7757d4ccd6ec
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-06 22:55:11 +0530

    BackTracking

--- src/RecursionRevision/WordSearch.java
+++
@@ -1,7 +0,0 @@
-package RecursionRevision;
-
-public class WordSearch {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/RecursionRevision/sudukosolver.java
@@ -0,0 +1,15 @@
+package RecursionRevision;
+
+public class sudukosolver {
+    public static void display(int[][] grid){
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid.length;j++){
+                System.out.println(grid[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
