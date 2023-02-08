9b54cfcb4fbb7aea8eba2f20542c75b2410c9300
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-08 18:39:00 +0530

    Recusion

--- src/Recusion_Doubt_Class/SubSet.java
+++ src/Recusion_Doubt_Class/SubSet.java
@@ -1,7 +1,11 @@
 package Recusion_Doubt_Class;
 
+import java.util.Scanner;
+
 public class SubSet {
     public static void main(String[] args) {
+      Scanner sc =new Scanner(System.in);
+      int n=sc.nextInt();
 
     }
 }
