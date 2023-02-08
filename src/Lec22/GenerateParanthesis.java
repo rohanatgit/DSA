58e8d3dab6ebc1117fbbd7f63781fbbb5118da9b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-08 19:18:31 +0530

    Recusion

--- src/Lec22/GenerateParanthesis.java
+++ src/Lec22/GenerateParanthesis.java
@@ -20,3 +20,25 @@ public class GenerateParanthesis {
         System.out.println(list);
     }
 }
+
+/*
+class Solution {
+    public static void genepara(int n,int opening,int closing,String ans,List<String>list){
+    if(opening==n && closing ==n){
+        list.add(ans);
+        return;
+    }
+   if(opening<n){
+      genepara(n,opening+1,closing,ans+"(",list);
+   }
+   if(closing<opening){
+      genepara(n,opening,closing+1,ans+")",list);
+   }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> list =new ArrayList<>();
+        genepara(n,0,0,"",list);
+        return list;
+    }
+}
+* */
