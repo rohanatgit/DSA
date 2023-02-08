9870f1731cadbdba021eaefeefc97e7e0a3e5067
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-08 22:29:56 +0530

    Recusion

--- src/Recusion_Doubt_Class/Recursion_Ascii_Subsequence.java
+++ src/Recusion_Doubt_Class/Recursion_Ascii_Subsequence.java
@@ -1,7 +1,26 @@
+
 package Recusion_Doubt_Class;
+
+import java.util.Scanner;
 
 public class Recursion_Ascii_Subsequence {
     public static void main(String[] args) {
+          Scanner sc =new Scanner(System.in);
+          int t=sc.nextInt();
+          while(t-->0){
+              String str=sc.next();
+          }
+    }
+    public static int printSubSeqnce(String ques,String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return 1;
+        }
 
+        char ch=ques.charAt(0);
+        int f1=printSubSeqnce(ques.substring(1),ans);
+        int f2=printSubSeqnce(ques.substring(1),ans+ch);
+        int f3=printSubSeqnce(ques.substring(1),ans+(int)ch);
+        return f1+f2+f3;
     }
 }
