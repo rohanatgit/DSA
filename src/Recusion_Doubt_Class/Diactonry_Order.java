24a0c936e9c6e23eb4bf06aaba368935be657807
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-08 19:56:55 +0530

    Recusion

--- src/Recusion_Doubt_Class/Diactonry_Order.java
+++ src/Recusion_Doubt_Class/Diactonry_Order.java
@@ -1,5 +1,23 @@
 package Recusion_Doubt_Class;
 
-public class Diactonry_Order {
+import java.util.Scanner;
 
+public class Diactonry_Order {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String str=sc.next();
+        printpermutation(str,"",str);
+    }
+    public static void printpermutation(String ques,String ans ,String org){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return ;
+        }
+
+        for(int i=0;i<ques.length();i++){
+            char ch=ques.charAt(i);
+            String ros=ques.substring(0,i)+ques.substring(i+1);
+            printpermutation(ros,ans+ch,org);
+        }
+    }
 }
