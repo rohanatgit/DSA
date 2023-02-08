546568f2260a0ba31854c89872e27be970538c25
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-08 19:40:55 +0530

    Recusion

--- src/Recusion_Doubt_Class/SubSet.java
+++ src/Recusion_Doubt_Class/SubSet.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SubSet {
+    static int c=0;
     public static void main(String[] args) {
       Scanner sc =new Scanner(System.in);
       int n=sc.nextInt();
@@ -11,17 +12,19 @@ public class SubSet {
      for(int i=0;i<arr.length;i++){
          arr[i]=sc.nextInt();
      }
-     ArrayList<Integer>ll=new ArrayList<>();
-     printSubset(arr,0,"");
+     int target= sc.nextInt();
+     printSubset(arr,0,"",0,target);
+        System.out.println(c);
     }
-    public static void printSubset(int[] arr,int i,String ans){
+    public static void printSubset(int[] arr,int i,String ans,int sum,int target){
         if(i==arr.length){
-            System.out.println(ans);
+            if(sum==target) {
+                System.out.println(ans);
+                c++;
+            }
             return ;
         }
-
-
-        printSubset(arr,i+1,ans+arr[i]+" ");
-        printSubset(arr,i+1,ans);
+        printSubset(arr,i+1,ans+arr[i]+" ",sum+arr[i],target);
+        printSubset(arr,i+1,ans,sum+arr[i],target);
     }
 }
