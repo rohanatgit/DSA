bdb5952c5b0035d351e67f9a1e9e10c5cded16ee
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-08 22:12:09 +0530

    Recusion

--- src/Recusion_Doubt_Class/Diactonry_Order.java
+++ src/Recusion_Doubt_Class/Diactonry_Order.java
@@ -1,17 +1,26 @@
 package Recusion_Doubt_Class;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Diactonry_Order {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         String str=sc.next();
-        printpermutation(str,"",str);
+        ArrayList<String>list=new ArrayList<>();
+        printpermutation(str,"",str,list);
+        Collections.sort(list);
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        System.out.println();
     }
-    public static void printpermutation(String ques,String ans ,String org){
+    public static void printpermutation(String ques,String ans ,String org,ArrayList<String>list){
         if(ques.length()==0){
             if(ans.compareTo(org)>0) {
-                System.out.println(ans);
+                //System.out.println(ans);
+                list.add(ans);
             }
             return ;
         }
@@ -19,7 +28,7 @@ public class Diactonry_Order {
         for(int i=0;i<ques.length();i++){
             char ch=ques.charAt(i);
             String ros=ques.substring(0,i)+ques.substring(i+1);
-            printpermutation(ros,ans+ch,org);
+            printpermutation(ros,ans+ch,org,list);
         }
     }
 }
