24d089818a057f31e4c1a479c6ca0a81ed70da2f
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-09 21:36:33 +0530

    Recusion

---
+++ src/Recusion_Assignment/Generate_Binary_Strings.java
@@ -0,0 +1,14 @@
+package Recusion_Assignment;
+
+import java.util.Scanner;
+
+public class Generate_Binary_Strings {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            String str=sc.next();
+
+        }
+    }
+}
