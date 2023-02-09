a13c6cea7771f969dcb6d46f5b27a7c4edb574ff
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-09 22:30:11 +0530

    Recusion

--- src/Recusion_Assignment/Odd_Even_Using_Recursion.java
+++ src/Recusion_Assignment/Odd_Even_Using_Recursion.java
@@ -1,7 +1,10 @@
 package Recusion_Assignment;
 
+import java.util.Scanner;
+
 public class Odd_Even_Using_Recursion {
     public static void main(String[] args) {
-
+       Scanner sc =new Scanner(System.in);
+       int n=sc.nextInt();
     }
 }
