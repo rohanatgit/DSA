b6bdcedd5ec7cab284e287e5118de4f5c6a4d7bf
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-09 22:19:27 +0530

    Recusion

--- src/Recusion_Assignment/Split_array.java
+++ src/Recusion_Assignment/Split_array.java
@@ -1,9 +1,15 @@
 package Recusion_Assignment;
 
+import java.util.Scanner;
+
 public class Split_array {
     public static void main(String[] args) {
     Scanner sc =new Scanner(System.in);
-
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+      }
     }
 
 }
