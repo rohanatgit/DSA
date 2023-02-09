7e6b7ad4b09afb8ce84fc9832f43fda70630277d
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-09 22:35:03 +0530

    Recusion

--- src/Recusion_Assignment/Odd_Even_Using_Recursion.java
+++ src/Recusion_Assignment/Odd_Even_Using_Recursion.java
@@ -6,5 +6,18 @@ public class Odd_Even_Using_Recursion {
     public static void main(String[] args) {
        Scanner sc =new Scanner(System.in);
        int n=sc.nextInt();
+       evenodd(n);
+    }
+    public static void evenodd(int n){
+        if(n==0){
+            return;
+        }
+        if(n%2!=0){
+            System.out.println(n);
+        }
+        evenodd(n-1);
+        if(n%2==0){
+            System.out.println(n);
+        }
     }
 }
