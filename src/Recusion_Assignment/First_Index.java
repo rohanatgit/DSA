27abd10e966e3203b0e32d638c4a9ab7dee87a3f
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-09 22:05:29 +0530

    Recusion

--- src/Recusion_Assignment/First_Index.java
+++ src/Recusion_Assignment/First_Index.java
@@ -1,7 +1,26 @@
 package Recusion_Assignment;
 
+import java.util.Scanner;
+
 public class First_Index {
     public static void main(String[] args) {
-
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int tar=sc.nextInt();
+        find(arr,0,arr.length-1,tar);
+    }
+    public static void find(int arr[],int lb,int ub,int tar){
+        if(lb>ub){
+            return ;
+        }
+        if(arr[lb]==tar){
+            System.out.println(lb+" ");
+            return ;
+        }
+        find(arr,lb+1,ub,tar);
     }
 }
