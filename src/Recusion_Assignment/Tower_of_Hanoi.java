42bc7aceb9bf19fadf4f250a7aa04e0e9d5b2d81
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-09 23:19:27 +0530

    Recusion

--- src/Recusion_Assignment/Tower_of_Hanoi.java
+++ src/Recusion_Assignment/Tower_of_Hanoi.java
@@ -2,6 +2,15 @@ package Recusion_Assignment;
 
 public class Tower_of_Hanoi {
     public static void main(String[] args) {
-
+        int n=3;
+        printstep(n,"src","hel","dis");
+    }
+    public static void printstep(int n,String s,String h,String d) {
+        if(n==0){
+            return ;
+        }
+        printstep(n-1,s,d,h);
+        System.out.println("Move "+n+"th disc from "+s+" to"+d);
+        printstep(n-1,h,s,d);
     }
 }
