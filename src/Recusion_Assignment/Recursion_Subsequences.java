9c2025c305aaadf8965b3568d0d3b6272c113a38
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-09 22:44:03 +0530

    Recusion

--- src/Recusion_Assignment/Recursion_Subsequences.java
+++ src/Recusion_Assignment/Recursion_Subsequences.java
@@ -1,7 +1,23 @@
 package Recusion_Assignment;
 
-public class Recursion_Subsequences {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Recursion_Subsequences {
+    static int count=0;
+    public static void main(String[] args) {
+      Scanner sc =new Scanner(System.in);
+      String str=sc.next();
+      subsequence(str,"");
+        System.out.println(count);
+    }
+    public static void subsequence(String ques,String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            count++;
+            return ;
+        }
+        char ch=ques.charAt(0);
+        subsequence(ques.substring(1),ans);
+        subsequence(ques.substring(1),ans+ch);
     }
 }
