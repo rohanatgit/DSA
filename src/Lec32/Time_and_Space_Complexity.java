c5d790f15c24389cf0a6d4a3790c7ed92874990e
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-10 19:11:43 +0530

    Time and Space Complexity

---
+++ src/Lec32/Time_and_Space_Complexity.java
@@ -0,0 +1,9 @@
+package Lec32;
+
+public class Time_and_Space_Complexity {
+    public static void main(String[] args) {
+        for(int i=0;i<100;i++){
+            System.out.println("hey");
+        }
+    }
+}
