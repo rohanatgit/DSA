4d5c1bd6a465e6ec828e5da784a98f428c4f109e
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-10 08:35:53 +0530

    Recusion

--- src/Lec31/Merge_Sort.java
+++ src/Lec31/Merge_Sort.java
@@ -2,8 +2,22 @@ package Lec31;
 
 public class Merge_Sort {
     public static void main(String[] args) {
-      int arr1[]={7,3,2,15,7,1};
-
+      int arr[]={7,3,2,15,7,1};
+      int[] ans=mergeSort(arr,0,arr.length-1);
+      for(int i=0;i<ans.length;i++){
+          System.out.print(ans[i]+" ");
+      }
+    }
+    public static int[] mergeSort(int arr[],int lo,int hi){
+        if(lo==hi){
+            int [] a=new int[1];
+            a[0]=arr[lo];
+            return a;
+        }
+        int mid=(lo+hi)/2;
+        int [] fs=mergeSort(arr,lo,mid);
+        int [] ss=mergeSort(arr,mid+1,hi);
+        return Merge_Ywo_Sorted_Array(fs,ss);
     }
     public static int[] Merge_Ywo_Sorted_Array(int[] arr1,int arr2[]){
         int n=arr1.length;
@@ -30,9 +44,11 @@ public class Merge_Sort {
             i++;
         }
         while(j<m){
-            ans[k]=arr2[k];
+            ans[k]=arr2[j];
             k++;
             j++;
         }
+        return ans;
     }
+
 }
