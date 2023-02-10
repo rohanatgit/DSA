7c43465290bf4ec84e7cdd94b08cbc3403e90983
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-10 10:12:12 +0530

    Recusion

--- src/Lec31/QuickSort.java
+++ src/Lec31/QuickSort.java
@@ -3,7 +3,11 @@ package Lec31;
 public class QuickSort {
     public static void main(String[] args) {
             int arr[]={9,1,3,4,67,8,8,9,90};
-
+       int idx=index(arr,0,arr.length-1);
+        System.out.print(idx);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
     public static int index(int[] arr,int lo,int hi){
         int last_element=arr[hi];
