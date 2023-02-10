d0d65fd507b6e61d7629d72a8e56a1fe484a7eb2
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-10 21:29:00 +0530

    Time and Space Complexity

--- src/Lec32/Time_and_Space_Complexity.java
+++ src/Lec32/Time_and_Space_Complexity.java
@@ -2,8 +2,32 @@ package Lec32;
 
 public class Time_and_Space_Complexity {
     public static void main(String[] args) {
-        for(int i=0;i<100;i++){
-            System.out.println("hey");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("hey");   //constant toime 100 baar chalega
         }
+        //Linear Search --> 0(N)
+        //Binary Search --> 0(NlogN)
+        int i = 0;
+        int n = 100;
+        while (i < n) {
+            System.out.println("nsasd");
+            i++;  // 0(N)
+        }
+        while(i<n){
+            System.out.println("Heyyy");
+            i*=2; //0(log(N))
+        }
+        while(n>0){
+            System.out.println("kjask");
+            n/=2;
+            //0(log(n))
+        }
+        while(i<=n){
+            System.out.println("hi");
+            i+=2;
+            i+=3;
+            //0(n)
+        }
+        
     }
 }
