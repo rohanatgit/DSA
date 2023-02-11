e9dd432ecbef207a35d5ab58b0cf03c841bf36da
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-11 17:10:47 +0530

    Time and Space Complexity

---
+++ src/Lec33/Rand_Quick_Sort.java
@@ -0,0 +1,51 @@
+package Lec33;
+
+import java.util.Random;
+
+public class Rand_Quick_Sort {
+    public static void quickSort(int[] arr,int lo,int hi){
+        if(lo>=hi){
+            return ;
+        }
+        int idx=partition(arr,lo,hi);
+        quickSort(arr,lo,idx-1);
+        quickSort(arr,idx+1,hi);
+    }
+    public static void main(String[] args) {
+        int si=10;
+        int ei=50;
+        int arr[]={9,1,56,-9,56,5,2,68,5,62,3,2,1,3,7};;;;;
+        quickSort(arr,0,arr.length-1);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void Random_number(int arr[],int si,int ei){
+        Random rand=new Random();
+        int lo=si;
+        int hi=ei;
+        int ri=rand.nextInt(hi-lo+1)+lo;
+        int temp=arr[ei];
+        arr[ei]=arr[ri];
+        arr[ri]=temp;
+    }
+    public static int partition(int[] arr,int lo,int hi){
+        Random_number(arr,lo,hi);
+        int pivot=arr[hi];
+        int idx=lo;
+        for(int i=lo;i<hi;i++){
+            if(arr[i]<=pivot){
+                //i ,idx
+                int t=arr[i];
+                arr[i]=arr[idx];
+                arr[idx]=t;
+                idx++;
+            }
+        }
+        //idx hi
+        int t=arr[idx];
+        arr[idx]=arr[hi];
+        arr[hi]=t;
+        return idx;
+    }
+}
