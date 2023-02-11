50b2799cbfea236bdc6e2b4141a4a880e76ffbc2
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-11 19:39:25 +0530

    Sorting

--- src/All_Sorting/Quick_Sort.java
+++ src/All_Sorting/Quick_Sort.java
@@ -1,7 +1,37 @@
 package All_Sorting;
 
 public class Quick_Sort {
-    public static void main(String[] args) {
+    public static void quick_Sort(int arr[],int lo,int hi){
+        if(lo>=hi){
+            return ;
+        }
+        int idx=partition(arr,lo,hi);
+        quick_Sort(arr,lo,idx-1);
+        quick_Sort(arr,idx+1,hi);
 
+    }
+    public static void main(String[] args) {
+        int arr[]={2,1,4,5,3};
+        quick_Sort(arr,0,arr.length-1);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static int partition(int arr[],int lo,int hi){
+        int pivot=arr[hi];
+        int idx=lo;
+        for(int i=lo;i<hi;i++){
+            if(arr[i]<=pivot){
+                //i,idx
+                int temp=arr[i];
+                arr[i]=arr[idx];
+                arr[idx]=arr[i];
+                idx++;
+            }
+        }
+        int temp=arr[idx];
+        arr[idx]=arr[hi];
+        arr[hi]=temp;
+        return  idx;
     }
 }
