7ebeb1761e04ed5479179866882e160374593235
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-12 00:31:33 +0530

    Palindrome_Substring

--- src/Lec34/Palindrome_Substring.java
+++ src/Lec34/Palindrome_Substring.java
@@ -2,6 +2,30 @@ package Lec34;
 
 public class Palindrome_Substring {
     public static void main(String[] args) {
-
+               String str="nitin";
+    }
+    public static int countPalidrome(String str){
+        int count=0;
+        for(int axis=0;axis<str.length();axis++){
+               for(int orbit=0;axis-orbit>=0 && axis+orbit<str.length();orbit++){
+                   if(str.charAt(axis-orbit)==str.charAt(axis+orbit)){
+                       count++;
+                   }
+                   else{
+                       break;
+                   }
+               }
+        }
+        for(double axis=0.5;axis<str.length();axis++){
+            for(double orbit=0.5;axis-orbit>=0 && axis+orbit<str.length();orbit++){
+                if(str.charAt((int)(axis-orbit))==str.charAt((int)(axis+orbit))){
+                    count++;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+      return count;
     }
 }
