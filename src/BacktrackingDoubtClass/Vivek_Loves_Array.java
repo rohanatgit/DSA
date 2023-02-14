37dcc0b5eddc21ff9c230666b0c5bf3af9e2ca95
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-14 18:16:22 +0530

    Recursion

--- src/BacktrackingDoubtClass/Vivek_Loves_Array.java
+++ src/BacktrackingDoubtClass/Vivek_Loves_Array.java
@@ -16,6 +16,21 @@ public class Vivek_Loves_Array {
         }
     }
     public static int Loves_Array(int [] arr,int si,int ei){
-
+             for(int mid=si;mid<arr.length;mid++){
+                  int s1=0;
+                  for(int i=si;i<=mid;i++){
+                       s1=s1+arr[i];
+                  }
+                    int s2=0;
+                  for(int i=mid+1;i<=ei;i++){
+                      s2+=arr[i];
+                  }
+                  if(s1==s2){
+                      int lf=Loves_Array(arr,si,mid);
+                      int rf =Loves_Array(arr,mid+1,ei);
+                      return Math.max(lf,rf)+1;//apna wala
+                  }
+             }
+             return 0;
     }
 }
