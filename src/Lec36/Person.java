eeaf3703ccd72d3515e22296d728e8b99bdc0379
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-17 16:21:10 +0530

    OOPS

---
+++ src/Lec36/Person.java
@@ -0,0 +1,10 @@
+package Lec36;
+
+public class Person {
+    String name;
+    int age;
+    public void fun(){
+        int Adhar_no=399098;
+        System.out.println(this.name+" "+this.age+" "+Adhar_no);
+    }
+}
