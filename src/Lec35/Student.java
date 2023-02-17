a44eb06a013895bc3bd05ab0bcde783fad81b956
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-17 10:31:18 +0530

    OOPS

--- src/Lec35/Student.java
+++ src/Lec35/Student.java
@@ -3,5 +3,7 @@ package Lec35;
 public class Student {
     String name;
     int age;
-
+    public void Intero_Yourself(){
+        System.out.println("helo rohan VArshney kya haal chal ");
+    }
 }
