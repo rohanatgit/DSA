5d04f855442947004ab7b256d5d5d0ea3b88639d
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-17 10:31:32 +0530

    OOPS

--- src/Lec35/Student_Client.java
+++ src/Lec35/Student_Client.java
@@ -1,5 +1,18 @@
 package Lec35;
 
-public class Student_Client {
+import java.util.Scanner;
 
+public class Student_Client {
+    public static void main(String[] args) {
+        Student s=new Student();
+        //new Student() ----> object new wale part obeject bhi instance bhi
+        s.age=90;
+        s.name="rohan";
+        System.out.println(s.age);
+        System.out.println(s.name);
+        s.age=89;
+        s.name="varshney";
+        System.out.println(s.age);
+        System.out.println(s.name);
+    }
 }
