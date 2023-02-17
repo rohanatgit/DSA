fea0d944d80b1cc86f9978d4df14f97ce0df84a9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-17 16:35:25 +0530

    OOPS

--- src/Lec36/Person.java
+++ src/Lec36/Person.java
@@ -3,8 +3,17 @@ package Lec36;
 public class Person {
     String name;
     int age;
-    public Person(){
-
+    //Constructor
+//    public Person(){
+//            this.name="Motu";
+//            this.age=56;   // non peramitrized Constructor
+//        //Constructor-> non return type
+//        //Class ka data member //name,age hai
+//    }
+    //Perametrized Constructor
+    public Person(String name,int age){
+        this.name=name;
+        this.age=age;
     }
     public void fun(){
         int Adhar_no=399098;
--- src/Lec36/Person_Client.java
+++ src/Lec36/Person_Client.java
@@ -2,11 +2,11 @@ package Lec36;
 
 public class Person_Client {
     public static void main(String[] args) {
-        Person p = new Person();
+        Person p = new Person("karan",67);
         p.name = "Kaju";
         p.age = 23;
         p.fun();
-        Person p1=new Person();
+        Person p1=new Person("bhadwa",76);
         p.name="Kunal";
         p.age=89;
 
