d3968b7d84c9526c2b761951e3a7f9b96c62bb0b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-18 18:52:35 +0530

    Queue

--- src/Lec37Stack/Queue.java
+++ src/Lec37Stack/Queue.java
@@ -2,14 +2,36 @@ package Lec37Stack;
 
 public class Queue {
     private int[] data;
-    private int front=0;
-    private int size=0;
+    private int front = 0;
+    private int size = 0;
 
-    public Queue(){
-        this.data=new int[5];
+    public Queue() {
+        this.data = new int[5];
     }
-    public Queue(int cap){
-        this.data=new int[cap];
+
+    public Queue(int cap) {
+        this.data = new int[cap];
     }
+
+    public boolean isEmpty(){
+        return this.size==0;
+    }
+
+    public boolean isFull(){
+        return this.size==this.data.length;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public void Enqueue(int item) throws Exception {
+        if(isFull()){
+            throw new Exception("sun pgl hai kya enqueue full ho gya hai bhai");
+        }
+          this.data[this.size]=item;
+          this.size++;
+    }
+
 
 }
