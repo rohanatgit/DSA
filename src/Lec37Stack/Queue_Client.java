aab45bc306fac42e90f7d688133ab31f26628168
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-18 19:52:19 +0530

    Queue

--- src/Lec37Stack/Queue_Client.java
+++ src/Lec37Stack/Queue_Client.java
@@ -1,8 +1,19 @@
 package Lec37Stack;
 
 public class Queue_Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Queue qq=new Queue();
         qq.Enqueue(10);
+        qq.Enqueue(20);
+        qq.Enqueue(30);
+        qq.Enqueue(40);
+        qq.Enqueue(50);
+        qq.dequeue();//10
+        qq.dequeue();//20
+        qq.Display();
+        qq.Enqueue(50);
+        qq.Enqueue(60);
+        qq.Enqueue(70);
+        qq.Display();
     }
 }
