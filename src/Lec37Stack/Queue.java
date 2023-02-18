e49e32a3fe28b57774bf8b5951ac16454f5b5ff8
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-18 18:45:53 +0530

    Queue

--- src/Lec37Stack/Queue.java
+++ src/Lec37Stack/Queue.java
@@ -3,5 +3,13 @@ package Lec37Stack;
 public class Queue {
     private int[] data;
     private int front=0;
+    private int size=0;
+
+    public Queue(){
+        this.data=new int[5];
+    }
+    public Queue(int cap){
+        this.data=new int[cap];
+    }
 
 }
