05ec04b18f0b4b27000c90029b1908c1ea7f405b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-02-22 20:33:49 +0530

    String capital

--- src/Contest.java
+++ src/Contest.java
@@ -1,7 +1,17 @@
 public class Contest {
     public static void main(String[] args) {
-        String str="hello world";
-        String ans="";
-
+        String str="hello world sahidoahsdfiihsd ohsdifia khsdihfi asodhfi jaosjdfoj josdj";
+        //String ans="";
+        String ans=Character.toUpperCase(str.charAt(0))+"";
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                ans+=" "+Character.toUpperCase(str.charAt(i+1));
+                i++;
+            }
+            else{
+                ans+=str.charAt(i);
+            }
+        }
+        System.out.println(ans);
     }
 }
