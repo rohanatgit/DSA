e0f3c07900276d7ccc7bfca09e9e14ca049444ae
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-07 01:52:53 +0530

    Stack

--- src/Lec38/Dynamic_Queue.java
+++ src/Lec38/Dynamic_Queue.java
@@ -1,9 +1,16 @@
 package Lec38;
 import Lec37Stack.Queue;
-public class Dynamic_Queue {
-    public void Enqueue(int item){
+public class Dynamic_Queue extends Queue{
+    public void Enqueue(int item) throws Exception {
         if(super.isFull()){
-
+             int arr[]=new int[2*super.data.length];
+             for(int i=0;i<super.data.length;i++){
+                 int idx=(this.front+1)%this.data.length;
+                 arr[i]=data[idx];
+             }
+             this.data=arr;
+             this.front=0;
         }
+        super.Enqueue(item);
     }
 }
