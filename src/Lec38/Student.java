34f988347ea197beb9a7fc59152448189cfed675
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-07 02:02:20 +0530

    Stack

---
+++ src/Lec38/Student.java
@@ -0,0 +1,14 @@
+package Lec38;
+
+public class Student {
+    String name;
+    int age;
+
+    public Student() {
+    }
+        public Student(String name,int age){
+            this.age=age;
+            this.name=name;
+        }
+}
+
