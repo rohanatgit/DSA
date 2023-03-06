27b11b62f8b5bf1a150dd13ac6fb3797e4dfd52b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-07 02:47:48 +0530

    Stack

---
+++ src/Lec39/Stack_Java.java
@@ -0,0 +1,21 @@
+package Lec39;
+
+import java.util.Stack;
+
+public class Stack_Java {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        st.push(60);
+        System.out.println(st.peek());
+        System.out.println(st);
+        System.out.println(st.pop());
+        System.out.println(st);
+        System.out.println(st.size());
+        st.isEmpty();
+    }
+}
