db08e803a2ea957b02c127586c4c0769d7110ce2
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-07 01:16:27 +0530

    Inheritance

--- src/Lec38/Client.java
+++ src/Lec38/Client.java
@@ -11,9 +11,36 @@ public class Client {
 
 
         //Case 2
-        p obj=new c();
-        System.out.println(obj.d);//1
-        System.out.println(obj.d1);//10
+//        p obj=new c();
+//        System.out.println(obj.d);//1
+//        System.out.println(obj.d1);//10
+//        System.out.println(((c)(obj)).d2);//20
+//        System.out.println(((c)(obj)).d);//2
+//        obj.fun();
+//        obj.fun1();
+//        ((c)(obj)).fun2();
+//
+//
+        //Case 3
+
+//        c obj =new p();
+//        System.out.println(obj.d);
+//        System.out.println(obj.d1);
+//        System.out.println(obj.d2);
+//        obj.fun();
+
+
+        //Case 4
+//        c obj =new c();
+//        System.out.println(obj.d);
+//        System.out.println(obj.d1);
+//        System.out.println(obj.d2);
+//        System.out.println(((p)obj).d);
+//        obj.fun();
+//        obj.fun1();
+//        obj.fun2();
+//
+//        JAVA MAI MULTIPLE INHERITANCE ALLOW NAHI HAI
 
     }
 }
