978cd2275702f12c901f69094656f94b25f033da
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-07 03:01:32 +0530

    Stack

--- src/Lec39/Stack_Reverse.java
+++ src/Lec39/Stack_Reverse.java
@@ -1,5 +1,18 @@
 package Lec39;
 
-public class Stack_Reverse {
+import java.util.Stack;
 
+public class Stack_Reverse {
+    public static void main(String[] args) {
+
+    }
+    public static void reverse(Stack<Integer> st){
+        if(st.isEmpty()){
+            return ;
+        }
+        int x=st.pop();
+        reverse(st);
+        st.push(x);
+
+    }
 }
