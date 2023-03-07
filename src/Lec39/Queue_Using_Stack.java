f839e2f4c04a072223b9a14556ada07026fc6349
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-08 05:16:05 +0530

    Queue

--- src/Lec39/Queue_Using_Stack.java
+++ src/Lec39/Queue_Using_Stack.java
@@ -1,4 +1,42 @@
 package Lec39;
 
-public class Queue_Using_Stack {
+import Lec38.Dynamic_Queue;
+import Lec38.Dynamic_Stack;
 
+public class Queue_Using_Stack {
+    private Dynamic_Stack data;
+    public  Queue_Using_Stack(){
+        this.data=new Dynamic_Stack();
+    }
+    public boolean isEmpty(){
+        return this.data.isEmpty();
+    }
+    public int size(){
+        return this.data.size();
+    }
+    public void Enqueue(int x) throws Exception {
+        this.data.push(x);
+    }
+    public int dequeue() throws Exception {
+        Dynamic_Stack temp=new Dynamic_Stack();
+        while(this.data.size()!=1){
+            temp.push(this.data.pop());
+        }
+        int x=this.data.pop();
+        while(temp.size()!=1){
+            this.data.push(temp.pop());
+        }
+        return x;
+    }
+    public int getfront() throws Exception {
+        Dynamic_Stack temp=new Dynamic_Stack();
+        while(this.data.size()!=0){
+            temp.push(this.data.pop());
+        }
+        int x=this.data.peek();
+        while(temp.size()!=1){
+            this.data.push(temp.pop());
+        }
+        return x;
+    }
+}
