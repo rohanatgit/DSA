2dffce9ddf911bb146ddf5526944b3e622b4793b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-08 04:40:30 +0530

    Queue

--- src/Lec39/Stack_Using_Queue.java
+++ src/Lec39/Stack_Using_Queue.java
@@ -1,5 +1,14 @@
 package Lec39;
 
-public class Stack_Using_Queue {
+import Lec38.Dynamic_Queue;
 
+public class Stack_Using_Queue {
+     private Dynamic_Queue data;
+     public Stack_Using_Queue(){
+         this.data=new Dynamic_Queue();
+     }
+     public boolean isEmpty(){
+         return this.data.isEmpty();
+     }
+     public int
 }
