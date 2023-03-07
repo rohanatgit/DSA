f84b1420da7086573f98af78512e131094d214d9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-07 21:01:44 +0530

    Stack

--- src/Lec39/Stack_Reverse.java
+++ src/Lec39/Stack_Reverse.java
@@ -4,6 +4,17 @@ import java.util.Stack;
 
 public class Stack_Reverse {
     public static void main(String[] args) {
+       Stack <Integer> st=new Stack<>();
+       st.push(10);
+       st.push(20);
+       st.push(30);
+       st.push(40);
+       st.push(50);
+        System.out.println(st);
+        System.out.println("**************");
+        reverse(st);
+        System.out.println(st);
+
 
     }
     public static void reverse(Stack<Integer> st){
@@ -13,6 +24,8 @@ public class Stack_Reverse {
         int x=st.pop();
         reverse(st);
         st.push(x);
+    }
+    public static void Insert_Down(Stack<Integer> st,int x){
 
     }
 }
