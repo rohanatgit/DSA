a879acf9c7f2f496cf8f2b7e2ed052d432675d29
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-08 04:55:42 +0530

    Queue

--- src/Lec39/Stack_Using_Queue.java
+++ src/Lec39/Stack_Using_Queue.java
@@ -3,29 +3,36 @@ package Lec39;
 import Lec38.Dynamic_Queue;
 
 public class Stack_Using_Queue {
-     private Dynamic_Queue data;
-     public Stack_Using_Queue(){
-         this.data=new Dynamic_Queue();
-     }
-     public boolean isEmpty(){
-         return this.data.isEmpty();
-     }
-     public int size(){
-         return this.data.size();
-     }
-     public void push(int item) throws Exception {
-         Dynamic_Queue temp=new Dynamic_Queue();
-         while(!this.data.isEmpty()){
-             temp.Enqueue(data.dequeue());
-         }
-         this.data.Enqueue(item);
-         while(!this.isEmpty()){
-             data.Enqueue(temp.dequeue());
-         }
-     }
-     public int pop() throws Exception {
-         return this.data.dequeue();
-     }
-     public int peek(){
-         return this.data.getFront();
-     }
+    private Dynamic_Queue data;
+
+    public Stack_Using_Queue() {
+        this.data = new Dynamic_Queue();
+    }
+
+    public boolean isEmpty() {
+        return this.data.isEmpty();
+    }
+
+    public int size() {
+        return this.data.size();
+    }
+
+    public void push(int item) throws Exception {
+        Dynamic_Queue temp = new Dynamic_Queue();
+        while (!this.data.isEmpty()) {
+            temp.Enqueue(data.dequeue());
+        }
+        this.data.Enqueue(item);
+        while (!this.isEmpty()) {
+            data.Enqueue(temp.dequeue());
+        }
+    }
+
+    public int pop() throws Exception {
+        return this.data.dequeue();
+    }
+
+    public int peek() {
+        return this.data.getFront();
+    }
+}
