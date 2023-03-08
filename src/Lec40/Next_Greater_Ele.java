1c4d04ff5fc75d34098284941a85295f22e22000
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-08 18:33:30 +0530

    Next_Greater_ELe

--- src/Lec40/Next_Greater_Ele.java
+++ src/Lec40/Next_Greater_Ele.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 public class Next_Greater_Ele {
     public static void main(String[] args) {
             int arr[]={11,3,23,9,15};
+            NGE(arr);
     }
     public static void NGE(int arr[]){
         int [] nge=new int[arr.length];
@@ -20,7 +21,10 @@ public class Next_Greater_Ele {
         }
         while(!st.isEmpty()){
             int ii=st.pop();
-            nge[ii]=-1
+            nge[ii]=-1;
+        }
+        for(int i=0;i<nge.length;i++){
+            System.out.println(arr[i]+" "+nge[i]);
         }
     }
 }
