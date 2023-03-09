fcc3cb876f13326cdd588a8640114a3ec065b9ea
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-09 11:19:19 +0530

    Next_Greater_ELe

---
+++ src/Lec40/Stock_Span.java
@@ -0,0 +1,13 @@
+package Lec40;
+
+public class Stock_Span {
+    public static void main(String[] args) {
+        int arr[]={91,11,13,15,38,42,37};
+    }
+    public static void Span_Cal(int arr[]){
+        int [] span=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            while()
+        }
+    }
+}
