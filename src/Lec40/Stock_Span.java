84eec6d0244731f9153ce73225ed657b54fa2a69
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-09 11:26:53 +0530

    Stock_Span

--- src/Lec40/Stock_Span.java
+++ src/Lec40/Stock_Span.java
@@ -1,13 +1,19 @@
 package Lec40;
 
+import java.util.Stack;
+
 public class Stock_Span {
     public static void main(String[] args) {
         int arr[]={91,11,13,15,38,42,37};
     }
     public static void Span_Cal(int arr[]){
         int [] span=new int[arr.length];
+        Stack<Integer> st =new Stack<>();
         for(int i=0;i<arr.length;i++){
-            while()
+            while( !st.isEmpty() && arr[i]>arr[st.peek()]){
+                st.pop();
+            }
+
         }
     }
 }
