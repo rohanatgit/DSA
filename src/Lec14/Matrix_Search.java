a412d2e89c9641f50fbb803e52bf2cd0cd05ba4f
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-10 22:55:49 +0530

    HIstogram

--- src/Lec14/Matrix_Search.java
+++ src/Lec14/Matrix_Search.java
@@ -3,32 +3,32 @@ package Lec14;
 import java.util.Scanner;
 
 public class Matrix_Search {
-    public static int Search(int arr[][],int x){
-        int row=0;
-        int col=arr[0].length-1;
-        while(col>=0 && row<arr.length){
-            if(arr[row][col]==x){
+    public static int Search(int arr[][], int x) {
+        int row = 0;
+        int col = arr[0].length - 1;
+        while (col >= 0 && row < arr.length) {
+            if (arr[row][col] == x) {
                 return 1;
-            }
-            else if(arr[row][col]<x){
+            } else if (arr[row][col] < x) {
                 row++;
-            }
-            else{
+            } else {
                 col--;
             }
         }
         return 0;
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        int[][] arr=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<arr[0].length;j++){
-                arr[i][j]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
-        int x=sc.nextInt();
-        System.out.println(Search(arr,x));
+        int x = sc.nextInt();
+        System.out.println(Search(arr, x));
     }
+}
