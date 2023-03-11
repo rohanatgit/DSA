fa88daee84611e7aa860e6bb348cb43b9219ab7a
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-11 13:25:27 +0530

    Stack

--- src/Stack/Balacked_Bracket.java
+++ src/Stack/Balacked_Bracket.java
@@ -1,7 +1,28 @@
 package Stack;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Balacked_Bracket {
     public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+       String str=sc.nextLine();
+       Stack<Character> st=new Stack<>();
+       for(int i=0;i<str.length();i++){
+           char ch=str.charAt(i);
+           if(ch=='(' || ch=='{' || ch=='['){
+
+           }
+           else if(ch==')'){
+
+           }
+           else if(ch=='}'){
+
+           }
+           else if(ch==']'){
+
+           }
+       }
 
     }
 }
