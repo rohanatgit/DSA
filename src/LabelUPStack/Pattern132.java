4f6b74cc265b4d5ccd82ff3a2c02640da826f592
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-11 17:05:56 +0530

    Stack

--- src/LabelUPStack/Pattern132.java
+++ src/LabelUPStack/Pattern132.java
@@ -2,6 +2,13 @@ package LabelUPStack;
 
 public class Pattern132 {
     public static void main(String[] args) {
-
+       int arr[]={3,1,4,2};
+    }
+    public boolean pattern(int arr[]){
+        int[]min=new int[arr.length];
+        min[0]=arr[0];
+        for(int i=1;i<min.length;i++){
+            min[i]=Math.min(min[i-1],arr[i]);
+        }
     }
 }
