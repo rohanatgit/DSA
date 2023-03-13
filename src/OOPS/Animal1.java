f45a4e3e0f8c8118518331bdf18300a81dbabf57
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-13 08:24:22 +0530

    OOPS

--- src/OOPS/Animal1.java
+++ src/OOPS/Animal1.java
@@ -3,11 +3,19 @@ package OOPS;
 public class Animal1 {
     String color;
     int age;
-
+    void initObj(String c,int a){
+        color=c;
+        age=a;
+    }
+    void display(){
+        System.out.println(color+" "+age);
+    }
     public static void main(String[] args) {
         Animal1 buzo =new Animal1();
-        buzo.color="black";
-        buzo.age=10;
-        System.out.println(buzo.color+" "+buzo.age);
+//        buzo.color="black";
+//        buzo.age=10;
+//        System.out.println(buzo.color+" "+buzo.age);
+        buzo.initObj("black",20);
+        buzo.display();
     }
 }
