b2aa34a03a3f36f82b4199ac8f172d0709ab6902
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-13 07:26:44 +0530

    OOPS

---
+++ src/OOPS/Animal.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+public class Animal {
+    public void eat(){
+        System.out.println("I am eating ");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Rohan 1");
+        Animal buzo=new Animal();
+        buzo.eat();
+    }
+}
