08cd93d6e75606ac6aadddf2f0bc316146a8d0b8
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-13 13:33:49 +0530

    OOPS

--- src/OOPS/Lec2Constructors/Test.java
+++ src/OOPS/Lec2Constructors/Test.java
@@ -1,7 +1,9 @@
 package OOPS.Lec2Constructors;
 
 public class Test {
+    int i;
     public static void main(String[] args) {
         Test t =new Test();
+        System.out.println(t.i);
     }
 }
