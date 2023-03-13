3e61faeee2cf653eefadf2bdbac33bb3410844a3
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-13 08:22:10 +0530

    OOPS

---
+++ src/OOPS/Animal1.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+public class Animal1 {
+    String color;
+    int age;
+
+    public static void main(String[] args) {
+        Animal1 buzo =new Animal1();
+        buzo.color="black";
+        buzo.age=10;
+
+    }
+}
