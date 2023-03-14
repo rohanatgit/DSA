54efaf46c8620a100309715fa651f4ef010ce109
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-14 18:35:01 +0530

    OOPS

--- src/OOPS/Lec3Inheritance/Animal.java
+++ src/OOPS/Lec3Inheritance/Animal.java
@@ -5,9 +5,9 @@ class Animal {
            System.out.println("I am Eating");
        }
 }
-class Dog extends Animal{
+class Dog extends Animal {
     public static void main(String[] args) {
-        Dog d=new Dog();
+        Dog d = new Dog();
         d.eat();
     }
-
+}
