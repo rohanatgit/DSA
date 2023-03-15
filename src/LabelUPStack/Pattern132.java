d50eb951d9f93d3a5ef38a02929689e42c4ddd7d
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-15 07:26:19 +0530

    LavelUP Stack

--- src/LabelUPStack/Pattern132.java
+++ src/LabelUPStack/Pattern132.java
@@ -1,5 +1,7 @@
 package LabelUPStack;
 
+import java.util.Stack;
+
 public class Pattern132 {
     public static void main(String[] args) {
         int arr[] = {3, 1, 4, 2};
@@ -11,5 +13,19 @@ public class Pattern132 {
         for (int i = 1; i < min.length; i++) {
             min[i] = Math.min(min[i - 1], arr[i]);
         }
+        Stack<Integer> st=new Stack<Integer>();
+        for(int j=arr.length-1;j>=0;j--){
+            int ith=min[j];
+            if(ith<arr[j]){
+                  while(!st.isEmpty() && arr[st.peek()]<=ith){
+                      st.pop();
+                  }
+                  if(!st.isEmpty() && arr[st.peek()]<arr[j]){
+                      return true;
+                  }
+            }
+            st.push(j);
+        }
+        return false;
     }
 }
