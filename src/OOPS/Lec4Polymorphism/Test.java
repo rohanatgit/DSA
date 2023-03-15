afd4e0237e936f04e5fd8362db253ab6a16438bc
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-16 01:16:56 +0530

    NGE

--- src/OOPS/Lec4Polymorphism/Test.java
+++ src/OOPS/Lec4Polymorphism/Test.java
@@ -4,8 +4,9 @@ public class Test {
     void show(int a){
         System.out.println("1");
     }
-    void show(){
+    String show(){
         System.out.println("2");
+        return "sdjn";
     }
     public static void main(String[] args) {
         Test t=new Test();
