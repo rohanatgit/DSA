a53c602fde829fa8b7cbfaa0e2e4796d0f886b44
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-17 19:02:12 +0530

    Opertor

---
+++ src/Postop.java
@@ -0,0 +1,49 @@
+public class Postop {
+    public static void main(String[] args) {
+//       int i=19,j=29 ,k;
+//       k=i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
+//        System.out.println(i);
+//        System.out.println(j);
+//        System.out.println(k);
+//
+//        int i=11;
+//        int j=--(i--);
+//        int m=0,n=0;
+//        int p=--m * --n * n-- * m--;
+//        System.out.println(p);
+//
+//        int a=1;
+//        a=a++ + ++a * --a - a--;
+//        System.out.println(a);
+
+//        int a=11++;
+//        System.out.println(a);
+
+//        int m=1010, n=1010;
+//        System.out.println(m++ / ++n * n-- /--m);
+//
+//        int x=001 , y=010 , z=100;
+//        int i=--x + y++ - z-- - --z + ++y - --x + y-- - --x;
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z);
+//        System.out.println(i);
+//
+//        int a = 010;
+//        System.out.println(a);
+
+
+//        int ch='A';
+//        System.out.println(ch++);// coercion
+
+//   char ch='A';
+//        System.out.println(++ch);
+//
+
+//        char ch="123".charAt(1);
+//        System.out.println(ch++ + ++ch);
+
+        char ch='0';
+        System.out.println(ch-- + --ch);
+    }
+}
