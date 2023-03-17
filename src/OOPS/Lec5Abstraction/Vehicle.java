f2868bee2d3213ebd1c415fc562db184f02c7331
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-17 08:03:44 +0530

    OOPS

---
+++ src/OOPS/Lec5Abstraction/Vehicle.java
@@ -0,0 +1,22 @@
+package OOPS.Lec5Abstraction;
+
+abstract class Vehicle {
+    abstract void start();
+}
+class car extends Vehicle{
+    void start(){
+        System.out.println("car start with the key");
+    }
+}
+class Scooter extends Vehicle{
+    void start(){
+        System.out.println("scooter starts with kick");
+    }
+
+    public static void main(String[] args) {
+        car c=new car();
+        c.start();
+        Scooter s=new Scooter();
+        s.start();
+    }
+}
