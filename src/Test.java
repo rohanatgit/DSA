4f2ddcb53a61aebc7bfc5259f5190b963086b5fa
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 15:43:28 +0530

    OOPs

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+//  void show(){
+//      System.out.println("lnks");
+//  }
+//}
+//class xyz extends Test{
+//    @Override
+//      void show(){
+//          System.out.println("lkznx");
+//
+//      }
+//
+//    public static void main(String[] args) {
+//        xyz x =new xyz();
+//        x.show();
+//    }
+    void show(){
+        System.out.println("kjsb");
+    }
+    void show(int a){
+        System.out.println("kajsb");
+    }
+}
