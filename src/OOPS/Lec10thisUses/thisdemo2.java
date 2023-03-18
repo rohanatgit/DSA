1df904e838c1184684df292e322818d8f9969b6e
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 10:30:43 +0530

    OOPs

--- src/OOPS/Lec10thisUses/thisdemo2.java
+++ src/OOPS/Lec10thisUses/thisdemo2.java
@@ -1,16 +1,19 @@
 package OOPS.Lec10thisUses;
 
 public class thisdemo2 {
-    void m1(){
-        Test t =new Test(this);
+    thisdemo2 m1() {
+        System.out.println("hello");
+        return this;
     }
+
     public static void main(String[] args) {
-        thisdemo2 td=new thisdemo2();
+        thisdemo2 td = new thisdemo2();
         td.m1();
     }
 }
-class Test{
-    Test(thisdemo2 td){
-        System.out.println("test class 2");
-    }
-}
+//}
+//class Test{
+//    Test(thisdemo2 td){
+//        System.out.println("test class 2");
+//    }
+//}
