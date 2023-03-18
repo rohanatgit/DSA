157db000e2ae1d289246b71eb8aede6f8a54ee6e
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 12:17:37 +0530

    OOPs

--- src/OOPS/Lec13StaticVariable/Employee.java
+++ src/OOPS/Lec13StaticVariable/Employee.java
@@ -1,8 +1,14 @@
 package OOPS.Lec13StaticVariable;
 
 public class Employee {
-    int emoid;
+    int empid;
     String name;
     String company;
+    void display(){
+        System.out.println(empid+" "+name+" "+company);
+    }
 
+    public static void main(String[] args) {
+
+    }
 }
