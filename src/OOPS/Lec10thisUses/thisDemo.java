a8386550dd65920cdb83b1e81d5f58405f96f1ee
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 10:20:21 +0530

    OOPs

--- src/OOPS/Lec10thisUses/thisDemo.java
+++ src/OOPS/Lec10thisUses/thisDemo.java
@@ -2,14 +2,14 @@ package OOPS.Lec10thisUses;
 
 public class thisDemo {
      thisDemo(){
+         this(10);
          System.out.println("hello BHai");
      }
      thisDemo(int a){
-         this();
+         //this();//current class contrutor
          System.out.println("parametrized construtor");
      }
-
     public static void main(String[] args) {
-        thisDemo td=new thisDemo(10);
+        thisDemo td=new thisDemo();
     }
 }
