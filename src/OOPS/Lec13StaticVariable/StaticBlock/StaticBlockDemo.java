3d2aa5ae070f72a8ebabb7c2f23ee045e6f80e82
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 16:24:25 +0530

    OOPs

--- src/OOPS/Lec13StaticVariable/StaticBlock/StaticBlockDemo.java
+++ src/OOPS/Lec13StaticVariable/StaticBlock/StaticBlockDemo.java
@@ -3,7 +3,10 @@ package OOPS.Lec13StaticVariable.StaticBlock;
 public class StaticBlockDemo {
     static {
         System.out.println("Hello");
-        System.exit(0);
+//        System.exit(0);
+    }
+    static{
+        System.out.println("fello");
     }
 
     public static void main(String[] args) {
