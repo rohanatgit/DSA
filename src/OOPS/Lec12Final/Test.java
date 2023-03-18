bbbea57f48415830ab45cb7e76e57d2ed03576be
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 11:41:09 +0530

    OOPs

--- src/OOPS/Lec12Final/Test.java
+++ src/OOPS/Lec12Final/Test.java
@@ -1,5 +1,18 @@
 package OOPS.Lec12Final;
 
 public class Test {
+    final void m1(){
+        System.out.println("i am in class Demo");
+    }
+}
+class demo extends Test{
+    void m1(){
+        System.out.println("i am in class test");
+    }
+
+    public static void main(String[] args) {
+        demo o=new demo();
+        o.m1();
+    }
 
 }
