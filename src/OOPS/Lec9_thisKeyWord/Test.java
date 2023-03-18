f5dca7d76791ed729a6df4f21d6b0b384d07bc57
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 10:03:29 +0530

    OOPs

---
+++ src/OOPS/Lec9_thisKeyWord/Test.java
@@ -0,0 +1,18 @@
+package OOPS.Lec9_thisKeyWord;
+
+public class Test {
+    int i;
+    void setvalue(int x){
+        i=x;
+    }
+    void show(){
+        System.out.println(i);
+    }
+}
+class xyz{
+    public static void main(String[] args) {
+        Test t =new Test();
+        t.setvalue(10);
+        t.show();
+    }
+}
