04cccdc4df9b0be10c12b0bcb78f5af2602b72c6
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 12:16:27 +0530

    OOPs

---
+++ src/OOPS/Lec13StaticVariable/Employee.java
@@ -0,0 +1,8 @@
+package OOPS.Lec13StaticVariable;
+
+public class Employee {
+    int emoid;
+    String name;
+    String company;
+
+}
--- src/OOPS/Lec13StaticVariable/Test.java
+++ src/OOPS/Lec13StaticVariable/Test.java
@@ -1,8 +1,11 @@
 package OOPS.Lec13StaticVariable;
 
 public class Test {
-    static int a=10;
-    void m1(){
-        static int b=20;
+    int a=10;
+}
+class Demo{
+    public static void main(String[] args) {
+        Test t=new Test();
+        System.out.println(t.a);
     }
 }
