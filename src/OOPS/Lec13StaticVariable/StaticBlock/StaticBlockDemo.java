7cc4869c9277587fbc464c91f04ee6c9efe6a51d
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 16:20:14 +0530

    OOPs

---
+++ src/OOPS/Lec13StaticVariable/StaticBlock/StaticBlockDemo.java
@@ -0,0 +1,11 @@
+package OOPS.Lec13StaticVariable.StaticBlock;
+
+public class StaticBlockDemo {
+    static {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
