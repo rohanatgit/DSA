0c3585145ff1b1399cc411a1c2b94d044d222003
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 16:02:38 +0530

    OOPs

---
+++ src/OOPS/Lec13StaticVariable/Staticmethord/Test.java
@@ -0,0 +1,12 @@
+package OOPS.Lec13StaticVariable.Staticmethord;
+
+public class Test {
+    void display(){
+        System.out.println("1");
+    }
+
+    public static void main(String[] args) {
+        Test t =new Test();
+        t.display();
+    }
+}
