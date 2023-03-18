347ea11075148ff6ce9f7b8e04336952b17398fe
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 10:12:47 +0530

    OOPs

--- src/OOPS/Lec10thisUses/thisDemo.java
+++ src/OOPS/Lec10thisUses/thisDemo.java
@@ -1,5 +1,17 @@
 package OOPS.Lec10thisUses;
 
 public class thisDemo {
+     int i;
+     void setValue(int i){
 
+     }
+     void show(){
+         System.out.println(i);
+     }
+
+    public static void main(String[] args) {
+        thisDemo td=new thisDemo();
+        td.setValue(100);
+        td.show();
+    }
 }
