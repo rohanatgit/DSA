8d143c7884ebc800a5a0fe4c2946ced5059db42b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 15:42:53 +0530

    OOPs

--- src/OOPS/Lec13StaticVariable/Employee.java
+++ src/OOPS/Lec13StaticVariable/Employee.java
@@ -3,12 +3,20 @@ package OOPS.Lec13StaticVariable;
 public class Employee {
     int empid;
     String name;
-    String company;
+    static String company;
+    Employee(int empid ,String name,String company){
+        this.empid=empid;
+        this.name=name;
+        this.company=company;
+    }
     void display(){
         System.out.println(empid+" "+name+" "+company);
     }
 
     public static void main(String[] args) {
-
+       Employee e1=new Employee(101,"rohan","Sp");
+       e1.display();
+       Employee e2=new Employee(102,"Deepka","SP");
+       e2.display();
     }
 }
