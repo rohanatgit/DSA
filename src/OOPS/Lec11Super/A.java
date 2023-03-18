c6a87bbdf569cdd33df3db7f81babe0e54a742ce
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 15:42:39 +0530

    OOPs

--- src/OOPS/Lec11Super/A.java
+++ src/OOPS/Lec11Super/A.java
@@ -4,15 +4,16 @@ public class A {
        A(){
           System.out.println(" i am in class A");
       }
+      int a=90;
 }
 class B extends A{
     B(){
         super();
+        System.out.println(super.a);
         System.out.println("B");
     }
 
     public static void main(String[] args) {
         B ob =new B();
-
     }
 }
