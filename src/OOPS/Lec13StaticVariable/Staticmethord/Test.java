a0182ea826b40d590928b9261ecb6ee1e4186d42
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-18 16:06:01 +0530

    OOPs

--- src/OOPS/Lec13StaticVariable/Staticmethord/Test.java
+++ src/OOPS/Lec13StaticVariable/Staticmethord/Test.java
@@ -1,12 +1,23 @@
 package OOPS.Lec13StaticVariable.Staticmethord;
 
 public class Test {
-    void display(){
+   static  void display(){
         System.out.println("1");
     }
 
     public static void main(String[] args) {
-        Test t =new Test();
-        t.display();
+//        Test t =new Test();
+//        t.display();
+        Test.display();//no need to create object
+    }
+}
+class Xyz{
+     void show(){
+        System.out.println("2");
+    }
+
+    public static void main(String[] args) {
+        Xyz x=new Xyz();
+        x.show();
     }
 }
