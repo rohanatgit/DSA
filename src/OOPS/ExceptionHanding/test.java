7f19d82291a08634943f507d73ee096b21bc0684
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-20 00:10:13 +0530

    Stack

---
+++ src/OOPS/ExceptionHanding/test.java
@@ -0,0 +1,14 @@
+package OOPS.ExceptionHanding;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("100/2");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+
+    }
+}
