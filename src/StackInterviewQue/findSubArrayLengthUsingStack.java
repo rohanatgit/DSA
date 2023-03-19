90ab717b162558370186491ff39457b3f6b33cca
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-19 19:27:25 +0530

    Stack

---
+++ src/StackInterviewQue/findSubArrayLengthUsingStack.java
@@ -0,0 +1,30 @@
+package StackInterviewQue;
+
+import java.util.Stack;
+
+public class findSubArrayLengthUsingStack {
+    public static int findSubArrayLengthUsing2(int arr[]) {
+        Stack<Integer> st = new Stack<>();
+        int min = arr.length;
+        int max = 0;
+        for(int i=0;i<arr.length;i++){
+            while(!st.isEmpty() && arr[i]<arr[st.peek()]){
+                min=Math.min(min,st.pop());
+            }
+            st.push(i);
+        }
+        st.clear();
+        for(int i=arr.length-1;i>=0;i--){
+            while(!st.isEmpty() && arr[i]>arr[st.peek()]){
+                max=Math.max(max,st.pop());
+            }
+            st.push(i);
+        }
+        return max-min+1;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={0,2,5,4,9,6,12};
+        System.out.println(findSubArrayLengthUsing2(arr));
+    }
+}
