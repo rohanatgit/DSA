6f57c2df0972be9c252de57e26821c64a6e42598
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-19 12:31:56 +0530

    Stack

--- src/StackInterviewQue/RevsreString.java
+++ src/StackInterviewQue/RevsreString.java
@@ -6,11 +6,13 @@ public class RevsreString {
     public static void main(String[] args) {
         Stack<Character> st =new Stack<>();
         String s="java";
+        char[] ch=s.toCharArray();
         for (int i=0;i<s.length();i++){
             st.push(s.charAt(i));
         }
-        while(!st.isEmpty()){
-            st.pop();
-        }
+       for(int i=0;i<s.length();i++){
+           ch[i]=st.pop();
+       }
+        System.out.println(String.valueOf(ch));
     }
 }
