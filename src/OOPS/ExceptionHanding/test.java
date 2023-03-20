299cad6d7faf591369362ec5ec52279a237bafc1
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-20 07:34:01 +0530

    OOPs

--- src/OOPS/ExceptionHanding/test.java
+++ src/OOPS/ExceptionHanding/test.java
@@ -1,14 +1,11 @@
 package OOPS.ExceptionHanding;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class test {
-    public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("1");
-        System.out.println(100/0);
-        System.out.println("5");
-        System.out.println("6");
-        System.out.println("7");
-        System.out.println("8");
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fis =new FileInputStream("sa");
 
     }
 }
