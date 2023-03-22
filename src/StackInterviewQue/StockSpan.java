ce99ee98b7c2199b1e5a3a8b1d479063beda9d0f
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-22 22:48:27 +0530

    Stack

--- src/StackInterviewQue/StockSpan.java
+++ src/StackInterviewQue/StockSpan.java
@@ -1,5 +1,25 @@
 package StackInterviewQue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StockSpan {
+     List<Integer> li =null;
+     public StockSpan(int price ){
+         li=new ArrayList<>();
+     }
+     public int next(int price){
+         li.add(price);
+         int c=0;
+         for(int i=li.size()-1;i>=0;i--){
+             if(li.get(i)<=price){
+                 c++;
+             }
+             else{
+                 break;
+             }
+         }
+         return c;
+     }
 
 }
