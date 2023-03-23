f27ea16cd5b3997c7bff1fb9f767c00197235001
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-23 07:47:26 +0530

    Stack

---
+++ src/StackInterviewQue/RevereseStack.java
@@ -0,0 +1,13 @@
+package StackInterviewQue;
+
+import java.util.Stack;
+
+public class RevereseStack {
+    public static void reverse(Stack<Integer> st){
+        if(st.isEmpty()){
+            return ;
+        }
+        int temp=st.pop();
+
+    }
+}
