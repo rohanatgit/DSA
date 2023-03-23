7b294ed8c8712a7e75e2498042a21c0a999a332d
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-23 08:19:03 +0530

    Stack

--- src/StackInterviewQue/SortStack.java
+++ src/StackInterviewQue/SortStack.java
@@ -1,5 +1,13 @@
 package StackInterviewQue;
 
-public class SortStack {
+import java.util.Stack;
 
+public class SortStack {
+    public static void push(Stack<Integer> st){
+        if(st.isEmpty()){
+            return ;
+        }
+        int temp=st.pop();
+
+    }
 }
