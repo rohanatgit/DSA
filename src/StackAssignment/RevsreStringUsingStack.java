10ee87e48635023719f45e540a32624de5eab4cf
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-23 11:37:05 +0530

    Stack

--- src/StackAssignment/RevsreStringUsingStack.java
+++ src/StackAssignment/RevsreStringUsingStack.java
@@ -1,5 +1,7 @@
 package StackAssignment;
 
+import Lec30.StringBuilder;
+
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -8,7 +10,13 @@ public class RevsreStringUsingStack {
         Stack<Character> st =new Stack<>();
         Scanner sc =new Scanner(System.in);
         String str=sc.next();
+        for(int i=0;i<str.length();i++){
+            st.push(str.charAt(i));
+        }
+        StringBuilder sb =new StringBuilder();
+        while(!st.isEmpty()){
 
+        }
 
     }
 }
