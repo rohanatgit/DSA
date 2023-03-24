42d122aac8fc4cf085218b58b83a3a4c382d1978
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-25 03:00:20 +0530

    LInkedList

--- src/Lec41/LInkedList.java
+++ src/Lec41/LInkedList.java
@@ -42,13 +42,18 @@ public class LInkedList {
     }
     //get
     public int getfirst() throws Exception {
-       if(head==null){
+       if(this.head==null){
            throw new Exception("pgl LINKEDLIST Khali hai nikal yha se ");
        }
         return head.data;//head mai null hai toh null POINTER expception aayega
     }
-
-    //
+    //getlast
+    public int getlast() throws Exception {
+        if(this.head==null){
+            throw new Exception("pgl hai kya yaar tu ");
+        }
+        return this.tail.data;
+    }
     public void display(){
                               // tail ka next mai null hota hai
         Node temp=this.head;
