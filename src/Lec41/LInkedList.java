3cb7ca94ebc3a26bcfd2ae6a5ba5d4fc92eac0b3
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-25 00:48:56 +0530

    LInkedList

--- src/Lec41/LInkedList.java
+++ src/Lec41/LInkedList.java
@@ -12,5 +12,7 @@ public class LInkedList {
 
     private Node head;// Phle Node ka address btayega
     private int size;//Linked List m kitne node hai
-}
+    public void addfirst(int item){
+
+    }
 }
