0b72e441c47f8f1ed9ffc9beb4e4851221849269
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-25 00:41:51 +0530

    LInkedList

--- src/Lec41/LInkedList.java
+++ src/Lec41/LInkedList.java
@@ -1,14 +1,16 @@
 package Lec41;
 
 public class LInkedList {
-    public class Node{
+    public class Node {
         int data;
         Node next;
-        Node(int data){
-            this.data=data;
+
+        Node(int data) {
+            this.data = data;
         }
     }
+
     private Node head;// Phle Node ka address btayega
     private int size;//Linked List m kitne node hai
-
+}
 }
