a6f50a6d02a0630e74d9ce5db42289d2120bf641
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-25 02:31:37 +0530

    LInkedList

--- src/Lec41/LinkdedListClient.java
+++ src/Lec41/LinkdedListClient.java
@@ -1,7 +1,14 @@
 package Lec41;
 
+import java.util.LinkedList;
+
 public class LinkdedListClient {
     public static void main(String[] args) {
-
+      LinkedList ll =new LinkedList();
+      ll.addFirst(10);
+      ll.addFirst(20);
+      ll.addFirst(30);
+      ll.addFirst(40);
+      ll.display();
     }
 }
