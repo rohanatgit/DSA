84975b274f8b0610bd10369ab8b6df946e56035c
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-26 21:02:06 +0530

    LInkedList

--- src/Lec42/Linked_List_Cycle.java
+++ src/Lec42/Linked_List_Cycle.java
@@ -1,5 +1,15 @@
 package Lec42;
 
 public class Linked_List_Cycle {
+      public class Node{
+          int data;
+          Node next;
+          public Node(int data){
+              this.data=data;
+          }
+      }
+      private Node head;
+      private int size;
+      private Node tail;
 
 }
