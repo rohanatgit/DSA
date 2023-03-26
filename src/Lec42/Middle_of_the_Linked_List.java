12b4b46feca2783e45bd409b54851db504ebb249
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-26 20:47:07 +0530

    LInkedList

--- src/Lec42/Middle_of_the_Linked_List.java
+++ src/Lec42/Middle_of_the_Linked_List.java
@@ -1,4 +1,27 @@
 package Lec42;
 
 public class Middle_of_the_Linked_List {
+    public class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) {
+          this.val = val;
+      }
+      ListNode(int val, ListNode next) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+  class Sloution{
+        public ListNode middleNode(ListNode head){
+            ListNode fast=head;
+            ListNode slow=head;
+            while(fast!=null && fast.next!=null){
+                fast=fast.next.next;
+                slow =slow.next;
+            }
+            return slow;
+        }
+  }
 }
