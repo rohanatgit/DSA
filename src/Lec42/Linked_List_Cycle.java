e353318b45f4aaaf3aaf3c4a1915d90da9788f73
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-26 21:58:53 +0530

    LInkedList

--- src/Lec42/Linked_List_Cycle.java
+++ src/Lec42/Linked_List_Cycle.java
@@ -1,41 +1,58 @@
 package Lec42;
 
 public class Linked_List_Cycle {
-      public class Node{
-          int data;
-          Node next;
-          public Node(int data){
-              this.data=data;
-          }
-      }
-      private Node head;
-      private int size;
-      private Node tail;
-      public void addfirst(int item){
-          Node nn =new Node(item);
-          if(this.size==0){
-              this.head=nn;
-              this.tail=nn;
-              this.size++;
-          }
-          else{
-              nn.next=head.next;
-              this.head=nn;
-              this.size++;
-          }
-      }
-      public void addlast(int item){
-          if(this.size==0){
-              addfirst(item);
-          }
-          Node nn =new Node(item);
-          this.tail.next=nn;
-          this.tail=nn;
-          size++;
-      }
-   public void CreateCycle(){
-          this.tail.next =this.head;//circular
+    public class Node {
+        int data;
+        Node next;
 
-   }
+        public Node(int data) {
+            this.data = data;
+        }
+    }
 
+    private Node head;
+    private int size;
+    private Node tail;
+
+    public void addfirst(int item) {
+        Node nn = new Node(item);
+        if (this.size == 0) {
+            this.head = nn;
+            this.tail = nn;
+            this.size++;
+        } else {
+            nn.next = head.next;
+            this.head = nn;
+            this.size++;
+        }
+    }
+
+    public void addlast(int item) {
+        if (this.size == 0) {
+            addfirst(item);
+        }
+        Node nn = new Node(item);
+        this.tail.next = nn;
+        this.tail = nn;
+        size++;
+    }
+
+    public void CreateCycle() {
+        this.tail.next = this.head;//circular
+
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+       return true;
+    }
+public void display()
+{
+    Node temp=this.head;
+    while(temp!=null){
+        System.out.println(temp.data+"-->");
+        temp=temp.next;
+    }
+    System.out.println(".");
+  }
 }
