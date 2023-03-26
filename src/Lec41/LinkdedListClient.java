ef24ca8d08536fa25af363ebed53c91611422a83
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-26 14:44:14 +0530

    LInkedList

--- src/Lec41/LinkdedListClient.java
+++ src/Lec41/LinkdedListClient.java
@@ -5,14 +5,10 @@ import java.util.LinkedList;
 public class LinkdedListClient {
     public static void main(String[] args) throws Exception {
         LInkedList ll =new LInkedList();
-      ll.addfirst(10);
-      ll.addfirst(20);
-      ll.addfirst(30);
-      ll.addfirst(40);
-      ll.display();
-      ll.addlast(90);
-      ll.display();
-        System.out.println(ll.getfirst());
-        System.out.println(ll.getlast());
+        ll.addfirst(10);
+        ll.addfirst(20);
+        ll.addfirst(30);
+        ll.addfirst(40);
+        ll.display();
     }
 }
