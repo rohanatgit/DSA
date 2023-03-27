5accc28a910d68bbdd0496020146e12cef2692b0
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-28 02:07:44 +0530

    LInkedList

--- src/Lec43/ReverseLinkedList.java
+++ src/Lec43/ReverseLinkedList.java
@@ -1,5 +1,35 @@
 package Lec43;
 
-public class ReverseLinkedList {
+import Lec42.Sort_List;
 
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode  next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    class Solution{
+        public ListNode reverseList(ListNode head){
+            ListNode curr=head;
+            ListNode prev =null;
+            while(curr!=null){
+                ListNode ahead=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=ahead;
+            }
+            return prev;
+        }
+    }
 }
