6a5cc1fdf7c3d144e7d7a75bc57648269367d273
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-27 05:52:26 +0530

    LInkedList

--- src/Lec42/INtersection_of_two_Linked_List.java
+++ src/Lec42/INtersection_of_two_Linked_List.java
@@ -1,5 +1,34 @@
 package Lec42;
 
 public class INtersection_of_two_Linked_List {
-kj
+    public class ListNode {
+    int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+  public  class Solution{
+       public ListNode getIntersectionNode (ListNode headA,ListNode headB){
+           ListNode A =headA;
+           ListNode B =headB;
+           while (A!=B){
+               if(A==null)
+               {
+                   A=headB;
+               }
+               else{
+                   A=A.next;
+               }
+               if(B==null){
+                   B=headA;
+               }
+               else{
+                   B=B.next;
+               }
+           }
+           return A;
+       }
+    }
 }
