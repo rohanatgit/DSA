484cbd1db0087ad6f0f3e1cb8306792fd1755dc9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-30 10:54:23 +0530

    LInkedList

---
+++ src/LinkedListPrapCoading/PalindromeLinkedList.java
@@ -0,0 +1,17 @@
+package LinkedListPrapCoading;
+
+import java.util.Scanner;
+
+public class PalindromeLinkedList {
+    public class ListNode{
+        int val=0;
+        ListNode next=null;
+        ListNode(int val){
+            this.val=val;
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+
+    }
+}
