9c624b955366d01736f4b930a75e0b9d358024d7
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-30 15:43:23 +0530

    LInkedList

--- src/LinkedListPrapCoading/MergeksortedLINkedList.java
+++ src/LinkedListPrapCoading/MergeksortedLINkedList.java
@@ -1,5 +1,26 @@
 package LinkedListPrapCoading;
 
-public class MergeksortedLINkedList {
+import java.util.Scanner;
 
+public class MergeksortedLINkedList {
+   public static class ListNode {
+       int val = 0;
+       ListNode next = null;
+
+       ListNode(int val) {
+           this.val = val;
+       }
+
+   }
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        ListNode dummy=new ListNode (-1);
+        ListNode prev=dummy;
+        while(n-->0){
+            prev.next=new ListNode (sc.nextInt());
+            prev=prev.next;
+        }
+    }
 }
