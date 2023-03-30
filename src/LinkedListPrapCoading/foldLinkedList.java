edc1df664067a1546164b865c0af6a52517fd16c
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-30 11:43:00 +0530

    LInkedList

--- src/LinkedListPrapCoading/foldLinkedList.java
+++ src/LinkedListPrapCoading/foldLinkedList.java
@@ -10,7 +10,35 @@ public class foldLinkedList {
             this.val=val;
         }
     }
-
+public static ListNode midNode(ListNode head) {
+    if (head == null || head.next == null)
+    {
+        return head;
+    }
+    ListNode slow=head;
+    ListNode fast=head;
+    while(fast==null &&  fast.next==null)
+    {
+        slow=slow.next;
+        fast=fast.next;
+    }
+    return slow;
+}
+public static ListNode reverseLL(ListNode head){
+        if(head==null || head.next==null){
+            return head;
+        }
+     ListNode prev=null;
+        ListNode curr=head;
+        ListNode ford=null;
+        while(curr!=null){
+            ford=curr.next;//backup
+            curr.next=prev;
+            prev=curr;
+            curr=ford;
+        }
+        return prev;
+}
 
     static void printlist(ListNode node){
         while(node!=null) {
