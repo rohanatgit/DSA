41a989f895c0c2e286e3103f802924b7534881dd
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-30 11:36:27 +0530

    LInkedList

--- src/LinkedListPrapCoading/foldLinkedList.java
+++ src/LinkedListPrapCoading/foldLinkedList.java
@@ -1,7 +1,9 @@
 package LinkedListPrapCoading;
 
+import java.util.Scanner;
+
 public class foldLinkedList {
-    public class ListNode{
+    public static class ListNode{
         int val=0;
         ListNode next=null;
         ListNode(int val){
@@ -9,5 +11,22 @@ public class foldLinkedList {
         }
     }
 
+
+    static void printlist(ListNode node){
+        while(node!=null) {
+            System.out.println(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        ListNode dummy=new ListNode(-1);
+        ListNode prev=dummy;
+        while(n-->0){
+
+        }
+    }
 
 }
