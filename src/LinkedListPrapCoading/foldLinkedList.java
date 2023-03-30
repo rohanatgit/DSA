5bd83419b88946336d1f1908b14672b13dcf9aa9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-30 11:34:16 +0530

    LInkedList

---
+++ src/LinkedListPrapCoading/foldLinkedList.java
@@ -0,0 +1,13 @@
+package LinkedListPrapCoading;
+
+public class foldLinkedList {
+    public class ListNode{
+        int val=0;
+        ListNode next=null;
+        ListNode(int val){
+            this.val=val;
+        }
+    }
+
+
+}
