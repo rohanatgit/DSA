bb2442e251dbe46e0b4e9533aaf37763ec81698a
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-03-30 15:07:13 +0530

    LInkedList

--- src/LinkedListPrapCoading/sigregateEvenodd.java
+++ src/LinkedListPrapCoading/sigregateEvenodd.java
@@ -15,6 +15,24 @@ public class sigregateEvenodd {
         if(head==null || head.next==null){
             return head;
         }
+        ListNode dummyEven=new ListNode(-1);
+        ListNode dummyodd=new ListNode(-1);
+        ListNode eventail=dummyEven;
+        ListNode oddtail=dummyodd;
+        ListNode curr=head;
+        while(curr!=null){
+            if(curr.val%2!=0){//odd
+                oddtail.next=curr;
+                oddtail=oddtail.next;
+            }
+            else{
+                eventail.next=curr;
+                eventail=eventail.next;
+            }
+            curr=curr.next;
+        }
+        eventail.next=dummyodd.next;
+        return dummyEven.next;
     }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
