6af5366472999008224797d3dd6d2c077aee17a1
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-03 08:42:19 +0530

    LInkedList

--- src/LinkedListPrapCoading/reversekgroup.java
+++ src/LinkedListPrapCoading/reversekgroup.java
@@ -1,5 +1,30 @@
 package LinkedListPrapCoading;
 
-public class reversekgroup {
+import java.util.Scanner;
 
+public class reversekgroup {
+       public static class ListNode{
+           int val=0;
+           ListNode next=null;
+           ListNode (int val){
+               this.val=val;
+           }
+       }
+       public static void printList(ListNode node){
+           while(node !=null){
+               System.out.println(node.val+" ");
+               node =node .next;
+           }
+       }
+       public static ListNode createList(int n){
+           Scanner sc =new Scanner(System.in);
+
+           ListNode dummy=new ListNode (-1);
+           ListNode prev=dummy;
+           while(n-->0){
+               prev.next=new ListNode(sc.nextInt());
+               prev=prev.next;
+           }
+           return prev;
+       }
 }
