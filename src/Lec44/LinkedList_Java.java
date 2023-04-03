dcf0a6d702eb50557d257f6be4c74bc85e763d6f
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-03 21:43:02 +0530

    LInkedList

--- src/Lec44/LinkedList_Java.java
+++ src/Lec44/LinkedList_Java.java
@@ -1,5 +1,19 @@
 package Lec44;
 
-public class LinkedList_Java {
+import java.util.LinkedList;
 
+public class LinkedList_Java {
+    public static void main(String[] args) {
+        LinkedList<Integer> list =new LinkedList<>();
+        list.add(10);//Add last
+        list.add(20);
+        list.add(20);
+        list.add(1,20);
+        list.add(40);
+        list.add(50);
+        list.add(60);//add last
+        System.out.println(list);
+        System.out.println(list.remove());//remove first
+
+    }
 }
