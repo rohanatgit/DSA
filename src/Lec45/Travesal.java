0200656598006f76ea0ddfabd6eb98e815851584
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-05 21:57:00 +0530

    TREE

--- src/Lec45/Travesal.java
+++ src/Lec45/Travesal.java
@@ -1,5 +1,32 @@
 package Lec45;
 
-public class Travesal {
+import Lec44.BinaryTree;
 
+import java.util.Scanner;
+
+public class Travesal {
+    Scanner sc =new Scanner(System.in);
+    public class Node{
+        int data;
+        Node left;
+        Node right;
+    }
+    private Node root;
+     public Travesal(){
+         this.root=createTree();
+     }
+     private Node createTree(){
+         int item=sc.nextInt();
+         Node nn =new Node();
+         nn.data=item;
+         boolean hlc=sc.nextBoolean();
+         if(hlc==true){
+             nn.left=createTree();
+         }
+         boolean hrc=sc.nextBoolean();
+         if(hrc==true){
+             nn.right=createTree();
+         }
+         return nn;
+     }
 }
