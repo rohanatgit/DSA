19545905ef8298941d3dd01cb9031e1eb4edc9b3
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-06 07:26:03 +0530

    TREE

--- src/Lec45/Diameter_of_Binary_Tree.java
+++ src/Lec45/Diameter_of_Binary_Tree.java
@@ -13,5 +13,20 @@ public class Diameter_of_Binary_Tree {
            this.right = right;
        }
    }
-
+    class Solution {
+        public int diameterOfBinaryTree(TreeNode root) {
+            int ld=diameterOfBinaryTree(root.left);
+            int rd=diameterOfBinaryTree(root.left);
+            int sd=ht(root.left)+ht(root.right)+2;
+           return  Math.max(ld,Math.max(rd,sd));
+        }
+        private int ht(TreeNode node){
+            if(node==null){
+                return -1;
+            }
+            int lh=ht(node.left);
+            int rh=ht(node.right);
+            return Math.max(lh,rh)+1;
+        }
+    }
 }
