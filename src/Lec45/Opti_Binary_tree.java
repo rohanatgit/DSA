f01871496612ee58a35cd0c6cba12d242c7dd927
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-06 08:07:44 +0530

    TREE

--- src/Lec45/Opti_Binary_tree.java
+++ src/Lec45/Opti_Binary_tree.java
@@ -1,5 +1,17 @@
 package Lec45;
 
 public class Opti_Binary_tree {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 
 }
