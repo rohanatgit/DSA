b02b17483816d9c41dc0eb06aa8027ca41b448a2
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-06 07:21:23 +0530

    TREE

--- src/Lec45/Diameter_of_Binary_Tree.java
+++ src/Lec45/Diameter_of_Binary_Tree.java
@@ -1,5 +1,17 @@
 package Lec45;
 
 public class Diameter_of_Binary_Tree {
+    public class TreeNode {
+       int val;
+      TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
 
 }
