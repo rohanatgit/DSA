1e6211425886b3b8e72285c26f19937bcde4497f
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-07 12:05:15 +0530

    TREE

--- src/Lec46/LCA2.java
+++ src/Lec46/LCA2.java
@@ -6,36 +6,62 @@ public class LCA2 {
         TreeNode left;
         TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
+        TreeNode() {
         }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
     }
-  class Solution{
-        public TreeNode lca(TreeNode root,TreeNode p,TreeNode q){
-            if(root==null) {
+
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (find(root, p) == false || find(root, q) == false) {
                 return null;
             }
-            if(root==p || root==q){
+            return LCA(root, p, q);
+
+        }
+
+        public TreeNode LCA(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) {
+                return null;
+            }
+
+            if (root == p || root == q) {
                 return root;
             }
-            TreeNode left=lca(root.left,p,q);
-            TreeNode right=lca(root.right,p,q);
-            if(left!=null && right!=null){
+
+            TreeNode left = LCA(root.left, p, q);
+            TreeNode right = LCA(root.right, p, q);
+            if (left != null && right != null) {
                 return root;
             }
-            if(left!=null){
+            if (left == null) {
                 return right;
-            }
-            else{
+            } else {
                 return left;
             }
         }
-        public TreeNode find(TreeNode root,TreeNode p){
-            if(root==null){
-                return null;
+
+        public boolean find(TreeNode root, TreeNode p) {
+
+            if (root == null) {
+                return false;
             }
-            
+            if (root == p) {
+                return true;
+            }
+
+            return find(root.left, p) || find(root.right, p);
+
         }
     }
 }
-
