47b2c88a13a565cd684e91c3403f642eb84a19c9
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-07 07:02:42 +0530

    TREE

---
+++ src/Lec46/Sum_Root_to_Leaf_Numbers.java
@@ -0,0 +1,17 @@
+package Lec46;
+
+public class Sum_Root_to_Leaf_Numbers {
+    public class TreeNode {
+        int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+}
