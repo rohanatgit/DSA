e16ba2d2af68f61abd0c0f9435a354cd8134ab85
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-07 18:58:40 +0530

    TREE

--- src/Lec46/Symmetric_Tree.java
+++ src/Lec46/Symmetric_Tree.java
@@ -2,16 +2,35 @@ package Lec46;
 
 public class Symmetric_Tree {
     public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+        int val;
+        TreeNode left;
+        TreeNode right;
 
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+
+        }
+        public boolean Mirror(TreeNode root1,TreeNode root2){
+            if(root1==null && root2==null){
+                return true;
+            }
+            else if(root1==null || root2==null){
+                return false;
+            }
+
+    }
 }
