19ab352231dfc415aae389dbaff6dc09f1dd5a49
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-07 11:53:19 +0530

    TREE

--- src/Lec46/LCA2.java
+++ src/Lec46/LCA2.java
@@ -10,6 +10,26 @@ public class LCA2 {
             val = x;
         }
     }
-
+  class Solution{
+        public TreeNode lca(TreeNode root,TreeNode p,TreeNode q){
+            if(root==null) {
+                return null;
+            }
+            if(root==p || root==q){
+                return root;
+            }
+            TreeNode left=lca(root.left,p,q);
+            TreeNode right=lca(root.right,p,q);
+            if(left!=null && right!=null){
+                return root;
+            }
+            if(left!=null){
+                return right;
+            }
+            else{
+                return left;
+            }
+        }
+    }
 }
 
