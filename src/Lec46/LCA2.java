7e1232e486bd0d03c73b13cc2f7015042ba45915
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-07 11:48:00 +0530

    TREE

--- src/Lec46/LCA2.java
+++ src/Lec46/LCA2.java
@@ -1,5 +1,15 @@
 package Lec46;
 
 public class LCA2 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 
 }
+
