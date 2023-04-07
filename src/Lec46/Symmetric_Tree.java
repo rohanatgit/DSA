e7dc55e1e4356164ff84b1747b0176513aed64bf
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-07 18:51:58 +0530

    TREE

---
+++ src/Lec46/Symmetric_Tree.java
@@ -0,0 +1,17 @@
+package Lec46;
+
+public class Symmetric_Tree {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+}
