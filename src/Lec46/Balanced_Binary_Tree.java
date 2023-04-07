9d1dc3a67a452371bdb36e98149ad7b54c8a3a5b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-07 08:04:00 +0530

    TREE

--- src/Lec46/Balanced_Binary_Tree.java
+++ src/Lec46/Balanced_Binary_Tree.java
@@ -1,4 +1,22 @@
 package Lec46;
 
 public class Balanced_Binary_Tree {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){
+
+        }
+        TreeNode(int val){
+            this.val=val;
+        }
+        TreeNode(int val,TreeNode left,TreeNode right){
+
+            this.val=val;
+            this.left=left;
+            this.right=right;
+        }
+    }
+
 }
