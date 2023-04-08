5b97d4dab1fa1ab5e9c8a9c8dc389402c5fd77ed
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-08 16:54:46 +0530

    TREE

--- src/Lec47/Binary_Search_Tree.java
+++ src/Lec47/Binary_Search_Tree.java
@@ -1,5 +1,14 @@
 package Lec47;
 
 public class Binary_Search_Tree {
+      public class Node{
+          int data;
+          Node left;
+          Node right;
+      }
+      private Node root;
+      public Binary_Search_Tree(int[] in){
+          this.root=CreateTree();
+      }
 
 }
