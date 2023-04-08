0b17a0569b7b54e7881c1832cf3eef2a0b0d373d
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-08 18:54:59 +0530

    TREE

---
+++ src/Lec47/Insert_Into_binary_Search_Tree.java
@@ -0,0 +1,19 @@
+package Lec47;
+
+public class Insert_Into_binary_Search_Tree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){};
+        TreeNode(int val){
+            this.val=val;
+        }
+        TreeNode(int val,TreeNode left,TreeNode right){
+            this.val=val;
+            this.left=left;
+            this.right=right;
+        }
+    }
+
+}
