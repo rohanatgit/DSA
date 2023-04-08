c82238f6f58048435593f251c915357ac0cd5723
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-08 17:04:34 +0530

    TREE

--- src/Lec47/Binary_Search_Tree.java
+++ src/Lec47/Binary_Search_Tree.java
@@ -11,7 +11,14 @@ public class Binary_Search_Tree {
           this.root=CreateTree(in,0,in.length-1);
       }
     private Node CreateTree(int[] in,int si,int ei){
-
-        return null;
+          if(si>ei){
+              return null;
+          }
+      int mid=(si+ei)/2;
+      Node nn =new Node();
+      nn.data=in[mid];
+      nn.left=CreateTree(in,si,mid-1);
+      nn.right=CreateTree(in,mid+1,ei);
+        return nn;
     }
 }
