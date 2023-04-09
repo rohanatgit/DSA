56943de99497f7e9d8c72584eb2ade809cf7ad3d
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-09 08:11:12 +0530

    TREE

---
+++ src/Lec47/Delete_Node_in_a_BST.java
@@ -0,0 +1,17 @@
+package Lec47;
+
+public class Delete_Node_in_a_BST {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+}
