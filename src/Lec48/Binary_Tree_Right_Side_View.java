8200f1e43ae986e85525d73a726434d2d73e92bf
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-12 02:28:13 +0530

    TREE

--- src/Lec48/Binary_Tree_Right_Side_View.java
+++ src/Lec48/Binary_Tree_Right_Side_View.java
@@ -1,5 +1,8 @@
 package Lec48;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Binary_Tree_Right_Side_View {
     public class TreeNode {
       int val;
@@ -13,5 +16,27 @@ public class Binary_Tree_Right_Side_View {
           this.right = right;
       }
   }
+    class Solution {
+        static int max_depth;
+        public List<Integer> rightSideView(TreeNode root) {
+            max_depth=-1;
+            List<Integer>ll=new ArrayList<>();
+            View(root,ll,0);
+            return ll;
+
+        }
+        public void View(TreeNode root,List<Integer>al,int curr_lev){
+            if(root==null){
+                return ;
+            }
+            if(curr_lev>max_depth){
+                al.add(root.val);
+                max_depth=curr_lev;
+            }
+            View(root.right,al,curr_lev);
+            View(root.left,al,curr_lev);
+
+        }
+    }
 
 }
