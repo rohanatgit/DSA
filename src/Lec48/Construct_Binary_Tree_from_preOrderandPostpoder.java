1b7d2086dabf8ce0f4b5834d1c3854b72e754c91
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-19 22:20:53 +0530

    TREE

---
+++ src/Lec48/Construct_Binary_Tree_from_preOrderandPostpoder.java
@@ -0,0 +1,17 @@
+package Lec48;
+
+public class Construct_Binary_Tree_from_preOrderandPostpoder {
+    public class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+     TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+}
