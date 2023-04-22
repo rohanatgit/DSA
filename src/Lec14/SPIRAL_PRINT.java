6e46212d90313b9d690390ab8d956ee7bc735f24
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-04-22 15:33:09 +0530

    TREE

--- src/Lec14/SPIRAL_PRINT.java
+++ src/Lec14/SPIRAL_PRINT.java
@@ -30,7 +30,7 @@ public class SPIRAL_PRINT {
          }
          minc++;
 
-    }
+
 
     public static void main(String[] args) {
         int[][] arr={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
