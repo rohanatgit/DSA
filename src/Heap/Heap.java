9d7361efb3259d0bf7113c31a5c0e50f6a55d145
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-14 09:55:17 +0530

    Heap

--- src/Heap/Heap.java
+++ src/Heap/Heap.java
@@ -12,10 +12,13 @@ public class Heap {
     private void upheapify(int ci) {
         int pi=(ci-1)/2;
         if(this.data.get(pi)>this.data.get(ci)){
-
+           swap(pi,ci);
         }
     }
     public void swap(int i,int j){
-
+       int ith=this.data.get(i);//20
+       int jth=this.data.get(j);//40
+        this.data.set(i,jth);
+        this.data.set(j,ith);
     }
 }
