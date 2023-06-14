0fdf5c324c5f37343fe726dd7e8360244ae2fd6a
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-14 22:04:06 +0530

    Heap

--- src/Heap/Kth_largest_Element_in_an_Array.java
+++ src/Heap/Kth_largest_Element_in_an_Array.java
@@ -12,7 +12,16 @@ public class Kth_largest_Element_in_an_Array {
         int[] arr = {2, 3, 4, 5, 2, 3, 45, 6, 7, 2, 9};
         int k = 3;
         for (int i = 0; i < k; i++) {
-            pq.add(arr[i]);
+            pq.add(arr[i]);//pq me add krega
         }
+        for(int i=k;i<arr.length;i++){
+            if(pq.peek()<arr[i]){//peek top ka value
+                pq.poll();//remove
+                pq.add(arr[i]);
+            }
+        }
+        System.out.println(pq.peek());
     }
 }
+//peek top ka element check karta hai
+//poll top ka element remove karta hai
