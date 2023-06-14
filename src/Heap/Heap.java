2689c8fe2606351095b2880c1ab0f13aa73ea56b
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-14 20:47:25 +0530

    Heap

--- src/Heap/Heap.java
+++ src/Heap/Heap.java
@@ -55,4 +55,17 @@ public class Heap {
     {
         System.out.println(this.data);
     }
+
+    public static void main(String[] args) {
+        Heap hp=new Heap();
+        hp.add(10);
+        hp.add(20);
+        hp.add(30);
+        hp.add(5);
+        hp.add(7);
+        hp.add(2);
+        System.out.println(hp.getmin());
+        hp.display();
+
+    }
 }
