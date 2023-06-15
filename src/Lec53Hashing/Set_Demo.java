a3829958567662391cec7f84e5a1e2a700db9082
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-15 19:58:49 +0530

    Heap

--- src/Lec53Hashing/Set_Demo.java
+++ src/Lec53Hashing/Set_Demo.java
@@ -1,7 +1,20 @@
 package Lec53Hashing;
 
+import java.util.HashSet;
+
 public class Set_Demo {
     public static void main(String[] args) {
+        HashSet<Integer>set=new HashSet<>();
+        set.add(90);
+        set.add(30);
+        set.add(40);
+        set.add(50);
+        set.add(60);
+        set.add(60);
+        System.out.println(set);
+
+        //remove
+        System.out.println(set.remove(-90));
 
     }
 }
