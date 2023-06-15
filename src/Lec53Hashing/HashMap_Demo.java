d23a79a79d936c1f1ebcaf25ffeda09ad34f9b3f
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-15 19:36:49 +0530

    Heap

--- src/Lec53Hashing/HashMap_Demo.java
+++ src/Lec53Hashing/HashMap_Demo.java
@@ -1,27 +1,35 @@
 package Lec53Hashing;
 
 import java.util.HashMap;
+import java.util.TreeMap;
 
 public class HashMap_Demo {
     public static void main(String[] args) {
-         HashMap<String,Integer> map=new HashMap<>();
-         //add
-        map.put("piya",90);
-        map.put("nitish",77);
-        map.put("akash",67);
-        map.put(null,88);
-        System.out.println(map);
+//         HashMap<String,Integer> map=new HashMap<>();
+//         //add
+//        map.put("piya",90);
+//        map.put("nitish",77);
+//        map.put("akash",67);
+//        map.put(null,88);
+//        System.out.println(map);
+//
+//        //get
+//        //0(1)
+//        System.out.println(map.get("Raj"));
+//
+//        System.out.println(map.get("Omika"));
+//
+//        //containsKey0(1)
+//        System.out.println(map.containsKey("akash"));
+//
+//        //remove 0(1)
+//        System.out.println(map.remove(map.remove("nitish")));
 
-        //get
-        //0(1)
-        System.out.println(map.get("Raj"));
-
-        System.out.println(map.get("Omika"));
-
-        //containsKey0(1)
-        System.out.println(map.containsKey("akash"));
-
-        //remove 0(1)
-        System.out.println(map.remove(map.remove("nitish")));
+        TreeMap<String,Integer>map1=new TreeMap<>();
+        //add
+        map1.put("sud",90);
+        map1.put("aksh singh",89);
+        map1.put("punnet",77);
+        System.out.println(map1);
     }
 }
