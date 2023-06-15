6b25db819f053bdc03ee1958e03d02d1245905e2
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-15 08:34:32 +0530

    Heap

--- src/Heap/Path_Sum3.java
+++ src/Heap/Path_Sum3.java
@@ -13,7 +13,27 @@ public class Path_Sum3 {
           this.right = right;
       }
   }
-    public static void main(String[] args) {
-
+    class Solution{
+        public int pathSum(TreeNode root,int targetSum){
+            if(root==null){
+                return 0;
+            }
+       int root_sum=countnumberofPath(root,targetSum);//10 ko include karke kitna path ayega
+            int left=pathSum(root.left,targetSum);
+            int right=pathSum(root.right,targetSum);
+            return root_sum+left+right;
+        }
+        public int countnumberofPath(TreeNode root,int targetSum){
+            if(root==null){
+                return 0;
+            }
+            int ans=0;
+            if(targetSum-root.val ==0){//leaf
+                ans++;
+            }
+            ans=ans+countnumberofPath(root.left,targetSum-root.val);
+            ans=ans+countnumberofPath(root.right,targetSum-root.val);
+            return ans;
+        }
     }
 }
