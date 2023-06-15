4cffae892c3d5f2e1b5b04b6c0400e220a1323d1
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-15 08:40:24 +0530

    Heap

--- src/Heap/Path_Sum3.java
+++ src/Heap/Path_Sum3.java
@@ -13,27 +13,35 @@ public class Path_Sum3 {
           this.right = right;
       }
   }
-    class Solution{
-        public int pathSum(TreeNode root,int targetSum){
-            if(root==null){
+    class Solution {
+        public int pathSum(TreeNode root, int targetSum) {
+
+            if (root == null) {
                 return 0;
             }
-       int root_sum=countnumberofPath(root,targetSum);//10 ko include karke kitna path ayega
-            int left=pathSum(root.left,targetSum);
-            int right=pathSum(root.right,targetSum);
-            return root_sum+left+right;
+
+            int root_sum = countnumberofpath(root, targetSum);// 10 Include
+
+            int left = pathSum(root.left, targetSum);
+            int right = pathSum(root.right, targetSum);
+            return root_sum + left + right;
+
         }
-        public int countnumberofPath(TreeNode root,int targetSum){
-            if(root==null){
+
+        public int countnumberofpath(TreeNode root, int targetSum) {
+
+            if (root == null) {
                 return 0;
             }
-            int ans=0;
-            if(targetSum-root.val ==0){//leaf
+            int ans = 0;
+            if (targetSum - root.val == 0) {
                 ans++;
             }
-            ans=ans+countnumberofPath(root.left,targetSum-root.val);
-            ans=ans+countnumberofPath(root.right,targetSum-root.val);
+            ans = ans + countnumberofpath(root.left, targetSum - root.val);
+            ans = ans + countnumberofpath(root.right, targetSum - root.val);
             return ans;
+
         }
+
     }
 }
