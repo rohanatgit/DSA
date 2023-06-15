e4e608d9d683e101dd1f20b57a030f00588621db
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-15 20:23:02 +0530

    Heap

--- src/Lec53Hashing/Set_Demo.java
+++ src/Lec53Hashing/Set_Demo.java
@@ -1,20 +1,32 @@
 package Lec53Hashing;
 
 import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Set_Demo {
     public static void main(String[] args) {
-        HashSet<Integer>set=new HashSet<>();
-        set.add(90);
-        set.add(30);
-        set.add(40);
-        set.add(50);
-        set.add(60);
-        set.add(60);
-        System.out.println(set);
+//        HashSet<Integer>set=new HashSet<>();
+//        set.add(90);
+//        set.add(30);
+//        set.add(40);
+//        set.add(50);
+//        set.add(60);
+//        set.add(60);
+//        System.out.println(set);
+//
+//        //remove
+//        System.out.println(set.remove(-90));
+//        System.out.println(set.contains(99));
+//
+//
+//
 
-        //remove
-        System.out.println(set.remove(-90));
+      //TreeSet
+      TreeSet<Integer>set1=new TreeSet<>();
+      set1.add(99);
+      set1.add(90);
+        System.out.println(set1);
 
     }
+
 }
