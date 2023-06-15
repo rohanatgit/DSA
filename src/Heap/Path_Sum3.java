a4edae7f97264eae959b9b7a827482fd00a7a3d7
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-15 08:20:21 +0530

    Heap

---
+++ src/Heap/Path_Sum3.java
@@ -0,0 +1,19 @@
+package Heap;
+
+public class Path_Sum3 {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public static void main(String[] args) {
+
+    }
+}
