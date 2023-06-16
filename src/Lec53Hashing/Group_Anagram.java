05082b2d2f27ac64553538516c8e653a2193c605
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-16 08:51:42 +0530

    Hashing

--- src/Lec53Hashing/Group_Anagram.java
+++ src/Lec53Hashing/Group_Anagram.java
@@ -9,6 +9,15 @@ public class Group_Anagram  {
 
     }
     public static List<List<String>>Anagram(String[] strs){
+        HashMap<String,ArrayList<String>>map=new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String s=strs[i];
+            String key=GenerateKey(s);
+            if(!map.containsKey(key)){
+                map.put(s,new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
 
     }
     public static String GenerateKey(String s){
@@ -18,5 +27,11 @@ public class Group_Anagram  {
             freq[ch-'a']=freq[ch-'a']+1;
 
         }
+        StringBuilder sb =new StringBuilder();
+        for(int i=0;i<freq.length;i++)
+        {
+            sb.append(freq[i]);
+        }
+        return sb.toString();
     }
 }
