412bd161293ebaf321478219a66be83175b88065
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-16 08:45:06 +0530

    Hashing

--- src/Lec53Hashing/Group_Anagram.java
+++ src/Lec53Hashing/Group_Anagram.java
@@ -1,4 +1,22 @@
 package Lec53Hashing;
 
-public class Group_Anagram {
+import java.util.*;
+
+public class Group_Anagram  {
+    public static void main(String[] args) {
+        String[] atr={"eat","tea","tan","ate","nat","bat"};
+
+
+    }
+    public static List<List<String>>Anagram(String[] strs){
+
+    }
+    public static String GenerateKey(String s){
+        int[ ] freq=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(0);
+            freq[ch-'a']=freq[ch-'a']+1;
+
+        }
+    }
 }
