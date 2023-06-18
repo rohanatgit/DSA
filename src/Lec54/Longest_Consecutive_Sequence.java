8ca6d9482d990984039ff51e6e3eba43e91d4388
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-18 13:18:59 +0530

    HashMap

--- src/Lec54/Longest_Consecutive_Sequence.java
+++ src/Lec54/Longest_Consecutive_Sequence.java
@@ -1,5 +1,23 @@
 package Lec54;
 
 public class Longest_Consecutive_Sequence {
-
+    public static void main(String[] args) {
+        int[] arr={2,3,4,6,5,1,8,9,7};
+    }
+    public static int LongestSequence(int[] arr){
+        HashMap<Integer,Boolean>map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i]-1)){
+                map.put(arr[i],false);
+            }
+            else{
+                map.put(arr[i],true);
+            }
+            if(map.containsKey(arr[i]+1)){
+                map.put(arr[i]+1,false);
+            }
+        }
+        int ans=0;
+        for(int key:map.)
+    }
 }
