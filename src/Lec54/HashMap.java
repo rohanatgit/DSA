db10c97be4c8b50b2eb4acb985b07b2687f83eec
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2023-06-18 11:27:29 +0530

    HashMap

--- src/Lec54/HashMap.java
+++ src/Lec54/HashMap.java
@@ -27,8 +27,28 @@ public class HashMap <K,V>{
     public HashMap(){
         this(4);
     }
+    public void put(K key,V value){
+        int bn=hashfunction(key);
+        Node temp=this.bucketarray.get(bn);//1st Address
+        while(temp!=null){
+            if(temp.key.equals(key)) {
+                temp.value=value;
+                return;
+            }
+            temp=temp.next;
+        }
+        Node nn =new Node (key,value);
+        temp=this.bucketarray.get(bn);
+        nn.next=temp;
+        this.bucketarray.set(bn,nn);
+        this.size++;
+    }
    public int hashfunction(K key){
-      int bn=key.hashCode();
+      int bn=key.hashCode() % this.bucketarray.size();
 
+      if(bn<0){
+          bn+=this.bucketarray.size();
+      }
+      return bn;
    }
 }
